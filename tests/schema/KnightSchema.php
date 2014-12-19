0d2362c69dd50484543ac2320f551acc3f7e1d86
Thomas LaFreniere <thomas@lafreniere.co>
2014-12-18 18:00:40 -0800

    Parent domain relationship

--- tests/schema/KnightSchema.php
+++ tests/schema/KnightSchema.php
@@ -22,8 +22,10 @@ class KnightSchema extends Schema {
         $table->addColumn("name", Type::String(false, "Mystery Knight", 50));
         $table->addColumn("birthdate", Type::DateTime());
 
-        $table->setConstraints(Constraint::PrimaryKey($table->columns->id));
-        $table->setConstraints(Constraint::ForeignKey($table->columns->swordid, SwordSchema::Table(), SwordSchema::Columns()->id));
+        $table->setConstraints(
+            Constraint::PrimaryKey($table->columns->id),
+            Constraint::ForeignKey($table->columns->swordid, SwordSchema::Table(), SwordSchema::Columns()->id)
+        );
 
         parent::__construct($table);
     }
