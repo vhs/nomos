e2fed257b44aa069b8877e8088be05d68f04d9f9
James Sapara <james.sapara@gmail.com>
2015-03-16 18:29:36 -0700

    mysql-server installs without interaction and with default password

--- tools/vagrant_provision.sh
+++ tools/vagrant_provision.sh
@@ -3,8 +3,10 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install python-software-properties
 sudo add-apt-repository --yes ppa:ondrej/php5-5.6
 sudo apt-get update
-sudo apt-get install --yes -q php5 php5-fpm php5-cli php5-mysqlnd
+echo "mysql-server-5.5 mysql-server/root_password password hackspace" | sudo debconf-set-selections
+echo "mysql-server-5.5 mysql-server/root_password_again password hackspace" | sudo debconf-set-selections
 sudo apt-get install --yes -q mysql-server mysql-client 
+sudo apt-get install --yes -q php5 php5-fpm php5-cli php5-mysqlnd
 sudo apt-get install --yes -q nginx
 
 # configure nginx
@@ -16,7 +18,6 @@ sudo service nginx restart
 cp /vagrant/conf/config.ini.php.template /vagrant/conf/config.ini.php
 
 # configure database
-mysqladmin -u root password hackspace
 # sudo sed -i 's/127.0.0.1/0.0.0.0/g' /opt/bitnami-nginxstack/mysql/my.cnf 
 mysql --host=localhost --user=root --password=hackspace -e 'CREATE DATABASE vhs_membership;'
 mysql --host=localhost --user=root --password=hackspace -e "GRANT ALL PRIVILEGES ON vhs_membership.* TO 'vhs_membership'@'127.0.0.1' IDENTIFIED BY 'password'; FLUSH PRIVILEGES;"
