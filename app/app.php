05c60e7e7ef5d96dd8a4c9a78314a237996c6f82
Thomas LaFreniere <thomas@lafreniere.co>
2015-04-14 15:04:21 -0700

    Server info

--- app/app.php
+++ app/app.php
@@ -12,7 +12,7 @@ require_once("include.php");
 
 $serverLog = (DEBUG) ? new \vhs\loggers\FileLogger(dirname(__FILE__) . "/server.log") : new \vhs\loggers\SilentLogger();
 
-\vhs\web\HttpContext::Init(new \vhs\web\HttpServer(null, $serverLog));
+\vhs\web\HttpContext::Init(new \vhs\web\HttpServer(new \vhs\web\modules\HttpServerInfoModule("Nomos"), $serverLog));
 
 \vhs\web\HttpContext::Server()->register(new \app\security\HttpApiAuthModule(\app\security\Authenticate::getInstance()));
 \vhs\web\HttpContext::Server()->register(new \vhs\web\modules\HttpBasicAuthModule("mmp", \app\security\Authenticate::getInstance()));
