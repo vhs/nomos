94311fe7a8c1cd4f621363db8c0a306b6f877ed4
James Sapara <james.sapara@gmail.com>
2015-08-15 17:48:57 -0700

    Front-end for membership

---
+++ web/admin/memberships/memberships.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular
+    .module('mmpApp.admin')
+    .config(['$stateProvider', function($stateProvider) {
+        $stateProvider
+            .state('admin.memberships', {
+                parent: "admin",
+                url: '/admin/memberships',
+                templateUrl: 'admin/memberships/memberships.html',
+                resolve: {
+                    memberships: ['MembershipService1', function(MembershipService1) {
+                        return MembershipService1.GetAll();
+                    }]
+                },
+                controller: ['$scope', 'memberships', function($scope, memberships) {
+                    $scope.memberships = memberships;
+                }]
+            });
+    }]);
