13c3818c637f2735513a01af44f2e97fe3c50a77
Thomas LaFreniere <thomas@lafreniere.co>
2015-08-17 20:34:03 -0700

    wip paypal handler

--- app/monitors/PaypalIpnMonitor.php
+++ app/monitors/PaypalIpnMonitor.php
@@ -9,6 +9,11 @@
 namespace app\monitors;
 
 use app\domain\Ipn;
+use app\domain\Membership;
+use app\domain\User;
+use app\security\PasswordUtil;
+use app\services\EmailService;
+use app\services\UserService;
 use vhs\Logger;
 use vhs\monitors\Monitor;
 
@@ -31,6 +36,81 @@ class PaypalIpnMonitor extends Monitor {
 
             $this->logger->log("We have a valid $ipn record, create a transaction, update users, etc");
 
+            $kvp = explode("&", $ipn->raw);
+
+            $raw = array();
+
+            foreach($kvp as $item)
+            {
+                $pair = explode("=", $item);
+
+                if (count($pair) == 2)
+                    $raw[$pair[0]] = $pair[1];
+            }
+
+            /* todo
+                the bare minimum is to email the admin (set to my email right now, but we should fix config -
+                I'll do an issue report now), and create an account on new user
+                so... get an IPN, compare paypal email with existing, if new, create new account, activate as
+                 member/friend, send admin email
+                if existing, set type to member/friend/keyholder(if allowed), extend expiry, activate user account,
+                email admin
+                a failed IPN should also email admin
+            */
+
+            $isExistingUser = false;
+
+            $user = User::findByEmail($ipn->payer_email);
+
+            $member = Membership::findByCode("member");
+
+            $userService = new UserService();
+
+            if (is_null($user)) {
+                $password = PasswordUtil::hash(
+                    substr(
+                        str_shuffle(
+                            str_repeat('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',16)
+                        ),0,16
+                    )
+                );
+
+                $fname = (in_array("first_name", $raw)) ? $raw["first_name"] : $ipn->payer_email;
+                $lname = (in_array("last_name", $raw)) ? $raw["last_name"] : $ipn->payer_email;
+
+                try {
+                    $user = $userService->Create(
+                        $ipn->payer_email,
+                        $password,
+                        $ipn->payer_email,
+                        $fname,
+                        $lname,
+                        $member->id
+                    );
+                } catch (\Exception $ex) {
+                    //this shouldn't happen... we should've found the user by email otherwise...
+                    $this->logger->log($ex->getMessage());
+                }
+
+                //email admin
+            } else {
+                $newMembership = Membership::findForPriceLevel($ipn->payment_amount);
+
+                if ($user->membership_id != $newMembership->id)
+                {
+                    //todo need to do a permission check on the user prob..
+                    $userService->UpdateMembership($user->id, $newMembership->id);
+                }
+
+                $user->active = "y";
+
+                //todo need to calc the new expiry date.
+                //$user->mem_expire = "";
+
+                $user->save();
+
+                //email user & admin
+            }
         }
     }
 
