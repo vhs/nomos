8ff73a6e13460e973134a0a68005dbcac622284e
Thomas LaFreniere <thomas@lafreniere.co>
2015-08-17 17:11:35 -0700

    Refactored payment gateways to be more testable

---
+++ app/gateways/IPaymentGateway.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Thomas
+ * Date: 8/17/2015
+ * Time: 4:12 PM
+ */
+
+namespace app\gateways;
+
+
+interface IPaymentGateway
+{
+    public function Name();
+    public function Process($data);
+}
---
+++ app/modules/HttpPaymentGatewayHandler.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Thomas
+ * Date: 8/17/2015
+ * Time: 4:16 PM
+ */
+
+namespace app\modules;
+
+
+use app\gateways\IPaymentGateway;
+use app\gateways\PaymentGatewayException;
+use vhs\web\HttpRequestHandler;
+use vhs\web\HttpServer;
+
+class HttpPaymentGatewayHandler extends HttpRequestHandler
+{
+    /** @var IPaymentGateway */
+    private $gateway;
+
+    public function __constructor(IPaymentGateway $gateway)
+    {
+        $this->gateway = $gateway;
+    }
+
+    public function handle(HttpServer $server)
+    {
+        $server->clear();
+        $server->code(200);
+
+        try
+        {
+            $server->output($this->gateway->Process($_REQUEST));
+        }
+        catch (PaymentGatewayException $ex)
+        {
+            if (DEBUG)
+                throw new \Exception($ex->getMessage());
+            else
+                throw new \Exception("Error occurred while processing payment gateway (".$this->gateway->Name().")");
+        }
+
+        $server->end();
+    }
+}
