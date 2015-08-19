237b17a312e3c2495991f04a028ac01c4a47b8d5
James Sapara <james.sapara@gmail.com>
2015-08-18 20:38:14 -0700

    mocked metrics complete

--- app/contracts/IMetricService1.php
+++ app/contracts/IMetricService1.php
@@ -12,24 +12,32 @@ interface IMetricService1 extends IContract {
 
     /**
      * @permission user
+     * @param start_range string iso date in UTC, if empty is start of today
+     * @param end_range string iso date in UTC, if empty is end of today
      * @return mixed
      */
     public function GetNewMembers($start_range, $end_range);
 
     /**
      * @permission user
+     * @param start_range string iso date in UTC, if empty is start of today
+     * @param end_range string iso date in UTC, if empty is end of today
      * @return mixed
      */
     public function GetNewKeyHolders($start_range, $end_range);
 
     /**
      * @permission user
+     * @param start_range string iso date in UTC, if empty is start of today
+     * @param end_range string iso date in UTC, if empty is end of today
      * @return mixed
      */
     public function GetTotalMembers($start_range, $end_range);
 
     /**
      * @permission user
+     * @param start_range string iso date in UTC, if empty is start of today
+     * @param end_range string iso date in UTC, if empty is end of today
      * @return mixed
      */
     public function GetTotalKeyHolders($start_range, $end_range);
--- web/user/home/home.js
+++ web/user/home/home.js
@@ -9,14 +9,29 @@ angular
       url: '/',
       templateUrl: 'user/home/home.html',
       resolve: {
-          membership: ['$stateParams', 'MembershipService1', function($stateParams, MembershipService1) {
-              return MembershipService1.Get($stateParams.membershipId);
+          newMembers: ['$stateParams', 'MetricService1', function($stateParams, MetricService1) {
+            return MetricService1.GetNewMembers(moment().utc().startOf('month').toISOString(), moment().utc().endOf('month').toISOString());
+          }],
+          totalMembers: ['$stateParams', 'MetricService1', function($stateParams, MetricService1) {
+              return MetricService1.GetTotalMembers(moment().utc().startOf('month').toISOString(), moment().utc().endOf('month').toISOString());
+          }],
+          newKeyHolders: ['$stateParams', 'MetricService1', function($stateParams, MetricService1) {
+              return MetricService1.GetNewKeyHolders(moment().utc().startOf('month').toISOString(), moment().utc().endOf('month').toISOString());
+          }],
+          totalKeyHolders: ['$stateParams', 'MetricService1', function($stateParams, MetricService1) {
+              return MetricService1.GetTotalKeyHolders(moment().utc().startOf('month').toISOString(), moment().utc().endOf('month').toISOString());
           }],
           allPermissions: ['PrivilegeService1', function(PrivilegeService1) {
               return PrivilegeService1.GetAllPrivileges();
           }]
       },
-      controller: ['$scope', function($scope) {
+      controller: [
+        '$scope', 'newMembers', 'newKeyHolders', 'totalMembers', 'totalKeyHolders', 
+        function($scope, newMembers, newKeyHolders, totalMembers, totalKeyHolders) {
+          $scope.newMembers = newMembers;
+          $scope.newKeyHolders = newKeyHolders;
+          $scope.totalMembers = totalMembers;
+          $scope.totalKeyHolders = totalKeyHolders;
           $scope.date = "January 2015";
       }]
     });
