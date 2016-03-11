cf292df4de72ed60eed146b234ab754c8a6c0769
Thomas LaFreniere <thomas@lafreniere.co>
2016-03-11 10:08:19 -0800

    security fix to prevent unauthorized access to privileges

--- app/domain/Privilege.php
+++ app/domain/Privilege.php
@@ -13,6 +13,8 @@ use app\schema\PrivilegeSchema;
 use vhs\database\wheres\Where;
 use vhs\domain\Domain;
 use vhs\domain\validations\ValidationResults;
+use vhs\security\CurrentUser;
+use vhs\security\exceptions\UnauthorizedException;
 
 class Privilege extends Domain {
     public static function Define() {
@@ -24,10 +26,16 @@ class Privilege extends Domain {
     }
 
     public static function findByCodes(...$code) {
+        if (!CurrentUser::hasAllPermissions("administrator") && !CurrentUser::hasAllPermissions(...$code) && !CurrentUser::canGrantAllPermissions(...$code))
+            throw new UnauthorizedException("Access denied");
+
         return Privilege::where(Where::In(Privilege::Schema()->Columns()->code, $code));
     }
 
     public static function findByCode($code) {
+        if (!CurrentUser::hasAllPermissions("administrator") && !CurrentUser::hasAllPermissions($code) && !CurrentUser::canGrantAllPermissions($code))
+            throw new UnauthorizedException("Access denied");
+
         $privs = Privilege::where(Where::Equal(Privilege::Schema()->Columns()->code, $code));
 
         if(count($privs) > 0)
