44040c8822516a3af8fd81e6676c6fc567f3dfe9
Thomas LaFreniere <thomas@lafreniere.co>
2019-01-30 16:20:25 -0800

    db console prompts

--- database/manager/web.js
+++ database/manager/web.js
@@ -13,64 +13,64 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server: server });
 
 wss.on("connection", ws => {
-    ws.on("message", message => {
-        const obj = JSON.parse(message);
-        console.log(message);
-        
-        if (obj.type === "connect") {
-            if (ws.mysql) {
-                ws.mysql.kill('SIGHUP');
-            }
-            
-            ws.mysql = spawn('script', ['-c', '"mysql -h 172.18.0.1 -u nomos -pnomos"'], { shell: true });
-            
-            ws.mysql.stdout.on('data', data => {
-                console.log(data.toString('utf8'));
-                ws.send(JSON.stringify({
-                    type: 'stdout',
-                    data: data.toString('utf8')
-                }));
-            });
-            
-            ws.mysql.stderr.on('data', data => {
-                console.log(data.toString('utf8'));
-                ws.send(JSON.stringify({
-                    type: 'stderr',
-                    data: data.toString('utf8')
-                }));
-            });
-            
-            ws.mysql.on('close', () => {
-                ws.send(JSON.stringify({
-                    type: 'close',
-                    data: data.toString('utf8')
-                }));
-                ws.mysql = null;
-            });
-            
-            return;
-        }
-        
-        if (obj.type === "command") {
-            console.log(obj.data);
-            if (ws.mysql) {
-                ws.mysql.stdin.write(obj.data);
-            }
-            
-            return;
-        }
-        
+  ws.on("message", message => {
+    const obj = JSON.parse(message);
+    console.log(message);
+      
+    if (obj.type === "connect") {
+      if (ws.mysql) {
+        ws.mysql.kill('SIGHUP');
+      }
+      
+      ws.mysql = spawn('script', ['-c', '"mysql -h 172.18.0.1 -u nomos -pnomos"'], { shell: true });
+      
+      ws.mysql.stdout.on('data', data => {
+        console.log(data.toString('utf8'));
         ws.send(JSON.stringify({
-            type: 'stderr',
-            data: data.toString('utf8')
+          type: 'stdout',
+          data: data.toString('utf8')
         }));
-    });
-    
-    ws.on("close", () => {
-        if (ws.mysql) {
-            ws.mysql.kill('SIGHUP');
-        }
-    });
+      });
+      
+      ws.mysql.stderr.on('data', data => {
+        console.log(data.toString('utf8'));
+        ws.send(JSON.stringify({
+          type: 'stderr',
+          data: data.toString('utf8')
+        }));
+      });
+      
+      ws.mysql.on('close', () => {
+        ws.send(JSON.stringify({
+          type: 'close',
+          data: 'program exit'
+        }));
+        ws.mysql = null;
+      });
+        
+      return;
+    }
+      
+    if (obj.type === "command") {
+      console.log(obj.data);
+      if (ws.mysql) {
+        ws.mysql.stdin.write(obj.data);
+      }
+      
+      return;
+    }
+      
+    ws.send(JSON.stringify({
+      type: 'stderr',
+      data: data.toString('utf8')
+    }));
+  });
+  
+  ws.on("close", () => {
+    if (ws.mysql) {
+      ws.mysql.kill('SIGHUP');
+    }
+  });
 });
 
 server.listen(3000);
