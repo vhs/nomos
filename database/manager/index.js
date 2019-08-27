aa3323e5ed126661a3597efe40b730a1cbb2b71a
Thomas LaFreniere <thomas@lafreniere.co>
2019-08-27 15:19:24 -0700

    database replication

--- database/manager/index.js
+++ database/manager/index.js
@@ -1,8 +1,48 @@
 const web = require("./web");
 const migrate = require("./migrate");
+const { master, replica } = require("./databases");
 
-migrate().then(() => {
+async function provision_master(db) {
+  const su = await db.sudo();
+  await su.query(`GRANT REPLICATION SLAVE ON *.* TO 'slave_user'@'%' IDENTIFIED BY 'password';`);
+  await su.query(`FLUSH PRIVILEGES;`);
+  // await master.query(`FLUSH TABLES WITH READ LOCK;`);
+  const status = await su.query(`SHOW MASTER STATUS;`);
+  const { File, Position } = status[0];
+  console.log(`SHOW MASTER STATUS: ${JSON.stringify(status)}`);
+
+  return { file: File, position: Position };
+}
+
+async function provision_slave(db, master, file, position) {
+  const su = await db.sudo();
+
+  const status = await su.query(`SHOW SLAVE STATUS;`);
+  console.log(`SHOW SLAVE STATUS; ${JSON.stringify(status)}`);
+
+  if (status.length < 1 || status[0].Master_Host !== "master") {
+    //await su.query(`CHANGE MASTER TO MASTER_HOST='${master.connection.host}',MASTER_USER='slave_user', MASTER_PASSWORD='password', MASTER_LOG_FILE='${file}', MASTER_LOG_POS=${position};`);
+    await su.query(`STOP SLAVE;`);
+    await su.query(`CHANGE MASTER TO MASTER_HOST='${master.connection.host}',MASTER_USER='slave_user', MASTER_PASSWORD='password', MASTER_AUTO_POSITION=1;`);
+    await su.query(`START SLAVE;`);
+  }
+}
+
+async function initialize() {
+  await master.connect();
+
+  const { file, position } = await provision_master(master);
+  await provision_slave(replica, master, file, position);
+
+  await replica.connect(); // slave replication will create default user
+  await migrate(master);
+}
+
+initialize().then(() => {
   web.server.listen(3000);
-  
+
   console.log("listening 3000...");
-}).catch(err => console.error(err));
+}).catch(err => {
+  console.error(err);
+  process.exit(1);
+});
--- database/manager/migrate.js
+++ database/manager/migrate.js
@@ -1,80 +1,45 @@
-const config = require("./config");
-
 const fs = require("fs");
-const { spawn } = require('child_process');
 
-let steps = fs.readdirSync("./migrations/");
-
-steps = steps
-.filter(step => Number.isInteger(Number(step)))
-.map(step => Number(step))
-.sort((a, b) => {
+const migrations = fs.readdirSync("./migrations/")
+  .filter(step => Number.isInteger(Number(step)))
+  .map(step => Number(step))
+  .sort((a, b) => {
     if (a < b) return -1;
     if (a > b) return 1;
-      
+
     return 0;
-});
-
-
-const mysql = require("mysql");
-const ManagedDatabase = require('./ManagedDatabase');
-
-// TODO host
-const host = "master";
-
-const db = new ManagedDatabase({
-    driver: mysql,
-    host: host,
-    user: config.db.user,
-    password: config.db.password,
-    database: config.db.database
-});
-
-console.log(JSON.stringify({
-    
-    host: host,
-    user: config.db.user,
-    password: config.db.password,
-    database: config.db.database
-}));
-
-console.log(JSON.stringify(steps));
-
-const execScript = scriptFile => {
-  return new Promise((resolve, reject) => {
-    const mysql = spawn('script', ['-c', `"mysql -h ${host} -D ${config.db.database} -u root -p${config.db.root_password} < ${scriptFile}"`], { shell: true });
-    
-    let output = "";
-    let error = "";
-    
-    mysql.stdout.on('data', data => {
-        output += data.toString('utf8');
-    });
-    
-    mysql.stderr.on('data', data => {
-        error += data.toString('utf8');
-    });
-    
-    mysql.on('error', err => {
-      error += err.toString();
-    });
-    
-    mysql.on('close', (code) => {
-      if (code != 0) {
-        reject(error);
-      } else {
-        resolve(output);
-      }
-    });
-    
   });
+
+console.log(`available migrations: ${JSON.stringify(migrations)}`);
+
+const migrateTo = async (db, version) => {
+  console.log(`starting migration for ${version}`);
+  const scripts = fs.readdirSync(`./migrations/${version}`);
+  console.log(`scripts: ${JSON.stringify(scripts)}`);
+
+  for(const scriptFile of scripts) {
+    console.log(`executing script: ${scriptFile}`);
+    // const script = fs.readFileSync(`./migrations/${version}/${scriptFile}`, 'utf8');
+
+    // console.log(`script: \n----\n${script}\n----`);
+
+    // await db.query(script);
+
+    const result = await db.execute(`./migrations/${version}/${scriptFile}`);
+    console.log(`result: ${result}`);
+
+    console.log(`success for: ${scriptFile}`);
+  }
+
+  console.log(`updating version to ${version}`);
+  await db.updateVersion(version);
 };
 
-const migrate = async () => {
-  await db.connect();
+const migrate = async (database) => {
+  const db = await database.sudo();
 
   const version = await db.version();
-  const desired = steps[steps.length - 1];
+  const desired = migrations[migrations.length - 1];
 
   console.log(`current version: ${version}`);
   console.log(`desired version: ${desired}`);
@@ -82,70 +47,47 @@ const migrate = async () => {
   if (version > desired) {
     throw new Error('FATAL manager is older than db schema version');
   }
-  
-  if (version < desired) {
-    steps = steps.filter(step => step > version);
-    
-    console.log(`Attempt migrations for steps ${version} -> [${Array.from(steps).join(" -> ")}]`);
-    
-    const migrateTo = async (v) => {
-      console.log(`starting migration for ${v}`);
-      const scripts = fs.readdirSync(`./migrations/${v}`);
-      console.log(`scripts: ${JSON.stringify(scripts)}`);
-      
-      for(const scriptFile of scripts) {
-        console.log(`executing script: ${scriptFile}`);
-        const script = fs.readFileSync(`./migrations/${v}/${scriptFile}`, 'utf8');
-        
-        // console.log(`script: \n----\n${script}\n----`);
-        
-        // await db.query(script);
-        
-        const result = await execScript(`./migrations/${v}/${scriptFile}`);
-        console.log(`result: ${result}`);
-        
-        console.log(`success for: ${scriptFile}`);
-      }
-      
-      console.log(`updating version to ${v}`);
-      await db.updateVersion(v);
-    };
-    
-    //console.log("START TRANSACTION;");
-   // await db.query("START TRANSACTION;");
-    
-    try {
-      for(const step of steps) {
-        await migrateTo(step);
-      }
-    } catch(err) {
-      console.error(err);
-      //console.log("ROLLING BACK!");
-      //await db.query("ROLLBACK;");
-      await db.disconnect();
-      
-      throw err;
-    }
-    
-    const latestVersion = Number(await db.version());
-  
-    if (latestVersion !== desired) {
-      const err = `post migration version : ${latestVersion} does not match desired version ${desired}`;
-      console.error(err);
-      
-      //console.log("ROLLING BACK!");
-      //await db.query("ROLLBACK;");
-      await db.disconnect();
-      
-      throw new Error(err);
-    }
-    
-   // await db.query('COMMIT;');
-    
-    console.log(`Successfully migrated to ${latestVersion}`);
+
+  if (version === desired) {
+    console.log(`Database already at ${desired} version`);
+
+    return;
   }
 
-  await db.disconnect();
-}
+  const steps = migrations.filter(step => step > version);
+
+  console.log(`Attempt migrations for steps ${version} -> [${Array.from(steps).join(" -> ")}]`);
+
+  //console.log("START TRANSACTION;");
+  // await db.query("START TRANSACTION;");
+
+  try {
+    for(const step of steps) {
+      await migrateTo(db, step);
+    }
+  } catch(err) {
+    console.error(err);
+    //console.log("ROLLING BACK!");
+    //await db.query("ROLLBACK;");
+
+    throw err;
+  }
+
+  const latestVersion = Number(await db.version());
+
+  if (latestVersion !== desired) {
+    const err = `post migration version : ${latestVersion} does not match desired version ${desired}`;
+    console.error(err);
+
+    //console.log("ROLLING BACK!");
+    //await db.query("ROLLBACK;");
+
+    throw new Error(err);
+  }
+
+  // await db.query('COMMIT;');
+
+  console.log(`Successfully migrated to ${latestVersion}`);
+};
 
 module.exports = migrate;
