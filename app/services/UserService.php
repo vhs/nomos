73a8f847e51b387c1623829d302d063c5083d518
Ty Eggen <tycho.eggen@gmail.com>
2020-11-24 16:19:46 -0800

    Initial working Stripe version

--- app/services/UserService.php
+++ app/services/UserService.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Thomas
@@ -19,53 +20,60 @@ use DateTime;
 use vhs\security\CurrentUser;
 use vhs\services\Service;
 
-class UserService extends Service implements IUserService1 {
+class UserService extends Service implements IUserService1
+{
 
-    public function GetUsers() {
+    public function GetUsers()
+    {
         return User::findAll();
     }
 
-    public function GetUser($userid) {
+    public function GetUser($userid)
+    {
         if (CurrentUser::getIdentity() == $userid || CurrentUser::hasAnyPermissions("administrator"))
             return User::find($userid);
 
         return null;
     }
 
-    public function UpdatePassword($userid, $password) {
+    public function UpdatePassword($userid, $password)
+    {
         $user = $this->GetUser($userid);
 
-        if(is_null($user)) return;
+        if (is_null($user)) return;
 
         $user->password = PasswordUtil::hash($password);
 
         $user->save();
     }
 
-    public function UpdateNewsletter($userid, $subscribe) {
+    public function UpdateNewsletter($userid, $subscribe)
+    {
         $user = $this->GetUser($userid);
 
-        if(is_null($user)) return;
+        if (is_null($user)) return;
 
         $user->newsletter = ($subscribe) ? true : false;
 
         $user->save();
     }
 
-    public function UpdateUsername($userid, $username) {
+    public function UpdateUsername($userid, $username)
+    {
         $user = $this->GetUser($userid);
 
-        if(is_null($user)) return;
+        if (is_null($user)) return;
 
         $user->username = $username;
 
         $user->save();
     }
 
-    public function UpdateName($userid, $fname, $lname) {
+    public function UpdateName($userid, $fname, $lname)
+    {
         $user = $this->GetUser($userid);
 
-        if(is_null($user) || CurrentUser::hasAnyPermissions("full-profile", "administrator") != true) return;
+        if (is_null($user) || CurrentUser::hasAnyPermissions("full-profile", "administrator") != true) return;
 
         $user->fname = $fname;
         $user->lname = $lname;
@@ -73,27 +81,41 @@ class UserService extends Service implements IUserService1 {
         $user->save();
     }
 
-    public function UpdateEmail($userid, $email) {
+    public function UpdateEmail($userid, $email)
+    {
         $user = $this->GetUser($userid);
 
-        if(is_null($user) || CurrentUser::hasAnyPermissions("full-profile", "administrator") != true) return;
+        if (is_null($user) || CurrentUser::hasAnyPermissions("full-profile", "administrator") != true) return;
 
         $user->email = $email;
 
         $user->save();
     }
 
-    public function UpdatePaymentEmail($userid, $email) {
+    public function UpdatePaymentEmail($userid, $email)
+    {
         $user = $this->GetUser($userid);
 
-        if(is_null($user) || CurrentUser::hasAnyPermissions("full-profile", "administrator") != true) return;
+        if (is_null($user) || CurrentUser::hasAnyPermissions("full-profile", "administrator") != true) return;
 
         $user->payment_email = $email;
 
         $user->save();
     }
 
-    public function UpdateCash($userid, $cash) {
+    public function UpdateStripeEmail($userid, $email)
+    {
+        $user = $this->GetUser($userid);
+
+        if (is_null($user) || CurrentUser::hasAnyPermissions("full-profile", "administrator") != true) return;
+
+        $user->stripe_email = $email;
+
+        $user->save();
+    }
+
+    public function UpdateCash($userid, $cash)
+    {
         $user = User::find($userid);
 
         if (is_null($user)) return;
@@ -103,7 +125,8 @@ class UserService extends Service implements IUserService1 {
         $user->save();
     }
 
-    public function Register($username, $password, $email, $fname, $lname) {
+    public function Register($username, $password, $email, $fname, $lname)
+    {
         if (User::exists($username, $email))
             throw new \Exception("User already exists");
 
@@ -120,19 +143,20 @@ class UserService extends Service implements IUserService1 {
         $user->save();
 
         $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-        $domainName = $_SERVER['HTTP_HOST'].'/';
+        $domainName = $_SERVER['HTTP_HOST'] . '/';
 
         $emailService = new EmailService();
 
         $emailService->EmailUser($user, 'welcome', [
             'token' => $user->token,
-            'host' => $protocol.$domainName
+            'host' => $protocol . $domainName
         ]);
 
         return $user;
     }
 
-    public function Create($username, $password, $email, $fname, $lname, $membershipid) {
+    public function Create($username, $password, $email, $fname, $lname, $membershipid)
+    {
         if (User::exists($username, $email)) {
             $this->context->log("wtf");
             throw new \Exception("User already exists");
@@ -153,25 +177,27 @@ class UserService extends Service implements IUserService1 {
 
         try {
             $this->UpdateMembership($user->id, $membershipid);
-        } catch(\Exception $ex) {}
+        } catch (\Exception $ex) {
+        }
 
         $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-        $domainName = $_SERVER['HTTP_HOST'].'/';
+        $domainName = $_SERVER['HTTP_HOST'] . '/';
 
         $emailService = new EmailService();
 
         $emailService->EmailUser($user, 'welcome', [
             'token' => $user->token,
-            'host' => $protocol.$domainName
+            'host' => $protocol . $domainName
         ]);
 
         return $user;
     }
 
-    public function RequestPasswordReset($email) {
+    public function RequestPasswordReset($email)
+    {
         $user = User::findByEmail($email)[0];
-        if(is_null($user)) {
-            return [ "success" => false, "msg" => "Unable to find a user by that e-mail address" ];
+        if (is_null($user)) {
+            return ["success" => false, "msg" => "Unable to find a user by that e-mail address"];
         }
 
         $request = new PasswordResetRequest();
@@ -182,21 +208,22 @@ class UserService extends Service implements IUserService1 {
         $request->save();
 
         $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-        $domainName = $_SERVER['HTTP_HOST'].'/';
+        $domainName = $_SERVER['HTTP_HOST'] . '/';
 
         $emailService = new EmailService();
         $emailService->EmailUser($user, 'recover', [
             'token' => $request->token,
-            'host' => $protocol.$domainName
+            'host' => $protocol . $domainName
         ]);
-        return [ "success" => true ];
+        return ["success" => true];
     }
 
-    public function ResetPassword($token, $password) {
+    public function ResetPassword($token, $password)
+    {
 
         $requests = PasswordResetRequest::findByToken($token);
 
-        if(!is_null($requests) && count($requests) == 1) {
+        if (!is_null($requests) && count($requests) == 1) {
             /** @var PasswordResetRequest $request */
             $request = $requests[0];
             $created = new DateTime($request->created);
@@ -212,7 +239,7 @@ class UserService extends Service implements IUserService1 {
                     $user->password = PasswordUtil::hash($password);
                     $user->save();
 
-                    return ["success" => true ];
+                    return ["success" => true];
                 }
             }
         } else {
@@ -228,35 +255,37 @@ class UserService extends Service implements IUserService1 {
                 return ["success" => true];
             }
         }
-        return [ "success" => false, "msg" => "Invalid Token" ];
+        return ["success" => false, "msg" => "Invalid Token"];
     }
 
-    public function PutUserPrivileges($userid, $privileges) {
+    public function PutUserPrivileges($userid, $privileges)
+    {
         $user = User::find($userid);
 
         $privArray = $privileges;
 
-        if(!is_array($privArray)) {
+        if (!is_array($privArray)) {
             $privArray = explode(",", $privileges);
         }
 
         $privs = Privilege::findByCodes(...$privArray);
 
-        foreach($user->privileges->all() as $priv)
+        foreach ($user->privileges->all() as $priv)
             $user->privileges->remove($priv);
 
-        foreach($privs as $priv)
+        foreach ($privs as $priv)
             $user->privileges->add($priv);
 
         $user->save();
     }
 
-    public function UpdateMembership($userid, $membershipid) {
+    public function UpdateMembership($userid, $membershipid)
+    {
         $user = User::find($userid);
 
         $membership = Membership::find($membershipid);
 
-        if(is_null($user) || is_null($membership)) {
+        if (is_null($user) || is_null($membership)) {
             throw new \Exception("Invalid user or membership type");
         }
 
@@ -275,9 +304,9 @@ class UserService extends Service implements IUserService1 {
     {
         $user = $this->GetUser($userid);
 
-        if(is_null($user) || CurrentUser::hasAnyPermissions("administrator") != true) return;
+        if (is_null($user) || CurrentUser::hasAnyPermissions("administrator") != true) return;
 
-        switch($status) {
+        switch ($status) {
             case "active":
             case "y":
             case "true":
@@ -308,17 +337,17 @@ class UserService extends Service implements IUserService1 {
     public function GetStatuses()
     {
         return array(
-            array("title"=>"Active", "code"=>"y"),
-            array("title"=>"Pending", "code"=>"t"),
-            array("title"=>"Inactive", "code"=>"n"),
-            array("title"=>"Banned", "code"=>"b")
+            array("title" => "Active", "code" => "y"),
+            array("title" => "Pending", "code" => "t"),
+            array("title" => "Inactive", "code" => "n"),
+            array("title" => "Banned", "code" => "b")
         );
     }
 
     public function AllowedColumns()
     {
-        
-        if(CurrentUser::hasAnyPermissions("grants") && !CurrentUser::hasAnyPermissions("administrator"))
+
+        if (CurrentUser::hasAnyPermissions("grants") && !CurrentUser::hasAnyPermissions("administrator"))
             return ["id", "username", "fname", "lname", "email"];
         else
             return null;
@@ -352,7 +381,8 @@ class UserService extends Service implements IUserService1 {
         $user->save();
     }
 
-    public function GetStanding($userid) {
+    public function GetStanding($userid)
+    {
         $user = $this->GetUser($userid);
 
         if (is_null($user)) return false;
@@ -368,7 +398,7 @@ class UserService extends Service implements IUserService1 {
      */
     public function GrantPrivilege($userid, $privilege)
     {
-        if(!CurrentUser::canGrantAllPermissions($privilege))
+        if (!CurrentUser::canGrantAllPermissions($privilege))
             return;
 
         $user = User::find($userid);
@@ -382,7 +412,7 @@ class UserService extends Service implements IUserService1 {
         if ($priv == null)
             return;
 
-        foreach($user->privileges->all() as $p) {
+        foreach ($user->privileges->all() as $p) {
             if ($p->code == $priv->code)
                 return;
         }
@@ -399,7 +429,7 @@ class UserService extends Service implements IUserService1 {
      */
     public function RevokePrivilege($userid, $privilege)
     {
-        if(!CurrentUser::canGrantAllPermissions($privilege))
+        if (!CurrentUser::canGrantAllPermissions($privilege))
             return;
 
         $user = User::find($userid);
@@ -414,7 +444,7 @@ class UserService extends Service implements IUserService1 {
 
         $remove = null;
 
-        foreach($user->privileges->all() as $p) {
+        foreach ($user->privileges->all() as $p) {
             if ($p->code == $priv->code)
                 $remove = $p;
         }
@@ -446,12 +476,13 @@ class UserService extends Service implements IUserService1 {
         return array_intersect($user->getPrivilegeCodes(), $me->getGrantCodes());
     }
 
-    public function RequestSlackInvite($email) {
+    public function RequestSlackInvite($email)
+    {
         $ch = curl_init('http://slack-invite:3000/invite');
         curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, 'email='.$email);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, 'email=' . $email);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
         curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
@@ -459,7 +490,7 @@ class UserService extends Service implements IUserService1 {
 
         $error = null;
 
-        if( !($response = curl_exec($ch)))
+        if (!($response = curl_exec($ch)))
             $error = "Error: Got " . curl_error($ch) . " when request slack invite for email: '" . $email . "'";
 
         curl_close($ch);
