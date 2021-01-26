135494f95d2b0e3b7bf7cc3f65dcafbd17170886
Ty Eggen <tycho.eggen@gmail.com>
2021-01-26 14:00:38 -0800

    Fixed docker-php-entrypoint path in docker_compose_run.sh

--- docker/docker_compose_run.sh
+++ docker/docker_compose_run.sh
@@ -4,4 +4,4 @@ docker_env_config.sh >/var/www/html/conf/env.php
 
 (cd /var/www/html/tools && php migrate.php -b -m)
 
-/usr/local/bin/docker_entry_point "$@"
+/usr/local/bin/docker-php-entrypoint "$@"
