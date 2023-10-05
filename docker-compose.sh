17baf598780406c12a9461d5f990d8386c25c1a4
Ty Eggen <tycho.eggen@gmail.com>
2023-10-05 18:12:42 +0000

    tools: improved handling for docker-compose wrapper script

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-export COMPOSE_FILE=$(cat docker-compose.conf | egrep -v '^(#|;|$)' | xargs | tr ' ' ':')
+HAS_COMPOSE_PLUGIN=0
 
-/usr/bin/env docker-compose $@
+if docker compose > /dev/null 2>&1 ; then
+    HAS_COMPOSE_PLUGIN=1
+fi
+
+COMPOSE_FILE_ARGS=$(grep -E -v '^(#|;|$)' docker-compose.conf | xargs -n 1 echo -f | xargs)
+
+if [ "${HAS_COMPOSE_PLUGIN}" = "1" ] ; then
+    # shellcheck disable=SC2086
+    docker compose ${COMPOSE_FILE_ARGS} "$@"
+else
+    # shellcheck disable=SC2086
+    /usr/bin/env docker-compose ${COMPOSE_FILE_ARGS} "$@"
+fi
