a0bcc0c65a129a5f56b65562dfec76781c803f4f
Ty Eggen <tycho.eggen@gmail.com>
2023-10-05 23:29:33 +0000

    schema: fix for incorrect default mem_expire value in users table

--- migrations/26/stripe.sql
+++ migrations/26/stripe.sql
@@ -1,9 +1,11 @@
+ALTER TABLE `users` CHANGE `mem_expire` `mem_expire` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
 ALTER TABLE `users` ADD COLUMN `stripe_id` VARCHAR(255) NULL;
 ALTER TABLE `users` ADD COLUMN `stripe_email` VARCHAR(255) NULL;
 
 CREATE TABLE IF NOT EXISTS `stripe_events` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ts` TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+  `ts` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `status` varchar(255) NOT NULL,
   `created` int(11) NOT NULL,
   `event_id` varchar(255) NOT NULL,
