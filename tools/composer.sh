eecb1d705d551a5c9eb82cad7c43ddcc48317330
Ty Eggen <tycho.eggen@gmail.com>
2025-01-05 02:19:16 +0000

    fix(dev): composer setup in composer.sh

--- tools/composer.sh
+++ tools/composer.sh
@@ -5,7 +5,7 @@ set -e
 cd "$(dirname "$(realpath "$0")")/../" || exit 255
 
 if [ ! -f ./tools/composer.phar ]; then
-    npm exec just setup composer
+    npm exec just setup vendor
 fi
 
 exec php ./tools/composer.phar "$@"
