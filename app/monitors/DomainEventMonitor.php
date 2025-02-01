3fdf19db2fb2661a02a3570f0d9b56b104126ef3
Ty Eggen <tycho.eggen@gmail.com>
2025-01-31 18:47:50 -0800

    feat: added support for more event types in DomainEventMonitor

--- app/monitors/DomainEventMonitor.php
+++ app/monitors/DomainEventMonitor.php
@@ -31,37 +31,88 @@ class DomainEventMonitor extends Monitor {
             /** @var Domain $domainClass */
             $domainClass = '\\app\\domain\\' . $event->domain;
 
+            /**
+             * Updated 2025/01/30 with all valid domain triggers and are returned via the GetEventTypes service method in the EventService1.
+             */
             switch ($event->event) {
-                /* Lets not support changed because it's super noisy. Throws for each field value change
-                 * case "changed":
-                    $domainClass::onAnyChanged(function(...$args) use ($event) {
+                case 'before:changed':
+                    /**
+                     * Intentionally disabled to prevent excessive event generation due to low level triggering.
+                     * I.e. Enabling this is noisy AF because it triggers on everything minor field update.
+                     **/
+                    // $domainClass::onAnyBeforeChange(function (...$args) use ($event) {
+                    //     $this->fireEvent($event, $args);
+                    // });
+                    break;
+
+                case 'before:created':
+                    MessageQueue::ensure($event->domain, $event->event);
+                    $domainClass::onAnyBeforeCreate(function (...$args) use ($event) {
                         $this->fireEvent($event, $args);
                     });
                     break;
-                */
+
+                case 'before:deleted':
+                    MessageQueue::ensure($event->domain, $event->event);
+                    $domainClass::onAnyBeforeDelete(function (...$args) use ($event) {
+                        $this->fireEvent($event, $args);
+                    });
+                    break;
+
+                case 'before:updated':
+                    MessageQueue::ensure($event->domain, $event->event);
+                    $domainClass::onAnyBeforeUpdate(function (...$args) use ($event) {
+                        $this->fireEvent($event, $args);
+                    });
+                    break;
+
+                case 'before:saved':
+                    MessageQueue::ensure($event->domain, $event->event);
+                    $domainClass::onAnyBeforeSave(function (...$args) use ($event) {
+                        $this->fireEvent($event, $args);
+                    });
+                    break;
+
+                case 'changed':
+                    /**
+                     * Intentionally disabled to prevent excessive event generation due to low level triggering.
+                     * I.e. Enabling this is noisy AF because it triggers on everything minor field update.
+                     **/
+                    // $domainClass::onAnyChanged(function (...$args) use ($event) {
+                    //     $this->fireEvent($event, $args);
+                    // });
+                    break;
+
                 case 'created':
                     MessageQueue::ensure($event->domain, $event->event);
                     $domainClass::onAnyCreated(function (...$args) use ($event) {
                         $this->fireEvent($event, $args);
                     });
                     break;
+
                 case 'deleted':
                     MessageQueue::ensure($event->domain, $event->event);
                     $domainClass::onAnyDeleted(function (...$args) use ($event) {
                         $this->fireEvent($event, $args);
                     });
                     break;
+
                 case 'updated':
                     MessageQueue::ensure($event->domain, $event->event);
                     $domainClass::onAnyUpdated(function (...$args) use ($event) {
                         $this->fireEvent($event, $args);
                     });
                     break;
+
                 case 'saved':
                     MessageQueue::ensure($event->domain, $event->event);
                     $domainClass::onAnySaved(function (...$args) use ($event) {
                         $this->fireEvent($event, $args);
                     });
+                    break;
+
+                default:
+                    $logger->log(sprintf('Invalid domain event [%s] for domain [%s]', $event->event, $event->domain));
                     break;
             }
         }
