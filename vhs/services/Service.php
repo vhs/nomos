9c87494b5dfedd5978af27b95923a1aa4a165f07
Ty Eggen <tycho.eggen@gmail.com>
2025-02-05 20:24:44 +0000

    feat: protected throwNotFound handler on Service

--- vhs/services/Service.php
+++ vhs/services/Service.php
@@ -9,11 +9,16 @@
 
 namespace vhs\services;
 
+/** @typescript */
 abstract class Service {
     /** @var ServiceContext $context */
     protected $context;
 
     public function __construct(ServiceContext $context = null) {
         $this->context = $context;
+    }
+
+    protected function throwNotFound(string $className) {
+        throw new \vhs\domain\exceptions\DomainException($className + ' not found', 404);
     }
 }
