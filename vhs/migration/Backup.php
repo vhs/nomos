6304dde37005321927eacb3371a0d6cac011e5d9
Ty Eggen <tycho.eggen@gmail.com>
2025-02-06 20:36:40 +0000

    refactor(backend): improved backup consistency

--- vhs/migration/Backup.php
+++ vhs/migration/Backup.php
@@ -36,26 +36,24 @@ class Backup {
     public function external_backup($do_host = false, $fileName = null, $backupPath = 'backup/') {
         $this->logger->log('Starting backup');
 
-        $fileName = !is_null($fileName) ? $fileName : 'db-backup-' . time() . '.sql';
+        $fileName = !is_null($fileName) ? $fileName : sprintf('db-backup-%s.sql', time());
 
         $command = [];
         $command[] = 'mysqldump';
-        $command[] = "-u '" . $this->user . "'";
-        $command[] = '-p' . $this->password;
+        $command[] = sprintf("-u '%s'", $this->user);
+        $command[] = sprintf("-p '%s'", $this->password);
         if ($do_host == true) {
-            $command[] = '--host ' . $this->server;
+            $command[] = '--host';
+            $command[] = $this->server;
+            $command[] = '--skip-ssl-verify-server-cert';
         }
-        $command[] = "'" . $this->database . "'";
+        $command[] = sprintf("'%s'", $this->database);
         $command[] = '>';
-        $command[] = "'" . $backupPath . $fileName . "'";
+        $command[] = sprintf("'%s%s'", $backupPath, $fileName);
 
         exec(implode(' ', $command), $output, $return);
 
-        if (!$return) {
-            return true;
-        } else {
-            return false;
-        }
+        return !$return;
     }
 
     public function internal_backup($fileName = null, $backupPath = 'backup/') {
