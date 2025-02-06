50c933501cda4644760223f9db510d078e87cee3
Ty Eggen <tycho.eggen@gmail.com>
2025-02-06 23:39:15 +0000

    fix(build): sessions directory permissions fix

--- docker/docker_compose_run.sh
+++ docker/docker_compose_run.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 # shellcheck shell=bash
 
+if [ ! -d /sessions ]; then
+    mkdir -p /sessions
+fi
+
+chown nobody:nobody /sessions && chmod 1777 /sessions
+
 /usr/local/bin/docker_env_config.sh
 
 (cd /var/www/html/tools && php migrate.php -b -m -t)
