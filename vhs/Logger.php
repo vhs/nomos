8033b64f5897bb59aa9e96bf0d1d114ab55f27ca
Ty Eggen <tycho.eggen@gmail.com>
2025-02-07 21:30:27 +0000

    feat(backend): debug logger method

--- vhs/Logger.php
+++ vhs/Logger.php
@@ -13,6 +13,12 @@ namespace vhs;
 abstract class Logger {
     abstract public function log($message);
 
+    public function debug($file, $line, $caller, $msg) {
+        if (\DEBUG === true) {
+            $this->log(sprintf('%s:%s - %s: %s', str_replace(BasePath::getBasePath(withSlash: true), '', $file), $line, $caller, $msg));
+        }
+    }
+
     public function __toString() {
         return get_called_class();
     }
