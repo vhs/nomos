dc28684e002d8e19e45e1b2ccb570b01094cc134
Ty Eggen <tycho.eggen@gmail.com>
2025-02-10 03:31:39 +0000

    build(frontend): refactor of vite config (ahem)

--- packages/frontend-react/vite.config.ts
+++ packages/frontend-react/vite.config.ts
@@ -1,58 +1,59 @@
 import fs from 'fs'
 import path from 'path'
 
-import "vite/client"
-
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [tsconfigPaths(), TanStackRouterVite(), react()],
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    'charts': ['chart.js', 'react-chartjs-2'],
-                    'crypto-js': ['crypto-js'],
-                    'moment': ['moment'],
-                    'react': ['react', 'react-dom'],
-                    'heroicons': ['@heroicons/react'],
-                    'react-spinners': ['react-spinners'],
-                    'react-toastify': ['react-toastify'],
-                    'swr': ['swr'],
-                    'tanstack': ['@tanstack/react-router'],
-                    'zod': ['zod']
+export default defineConfig(({ mode }) => {
+    return {
+        plugins: [tsconfigPaths(), TanStackRouterVite(), react()],
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        'charts': ['chart.js', 'react-chartjs-2'],
+                        'crypto-js': ['crypto-js'],
+                        'moment': ['moment'],
+                        'react': ['react', 'react-dom'],
+                        'heroicons': ['@heroicons/react'],
+                        'react-spinners': ['react-spinners'],
+                        'react-toastify': ['react-toastify'],
+                        'swr': ['swr'],
+                        'tanstack': ['@tanstack/react-router'],
+                        'zod': ['zod']
+                    }
                 }
             }
-        }
-    },
-    server: {
-        https: import.meta.env?.DEV
-            ? {
-                  ca: fs.readFileSync(path.resolve(__dirname, 'certs/ca.crt')),
-                  key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.key')),
-                  cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.crt'))
-              }
-            : {},
+        },
+        server: {
+            https:
+                mode === 'production'
+                    ? {}
+                    : {
+                          ca: fs.readFileSync(path.resolve(__dirname, 'certs/ca.crt')),
+                          key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.key')),
+                          cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.crt'))
+                      },
 
-        proxy: {
-            '/services': {
-                target: 'https://membership.devtest.vanhack.ca',
-                changeOrigin: true,
-                secure: false,
-                ws: true,
-                xfwd: true
-            },
-            '/storybook': {
-                target: 'http://localhost:6006',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/storybook/, ''),
-                secure: false,
-                ws: true,
-                xfwd: true
+            proxy: {
+                '/services': {
+                    target: 'https://membership.devtest.vanhack.ca',
+                    changeOrigin: true,
+                    secure: false,
+                    ws: true,
+                    xfwd: true
+                },
+                '/storybook': {
+                    target: 'http://localhost:6006',
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/storybook/, ''),
+                    secure: false,
+                    ws: true,
+                    xfwd: true
+                }
             }
         }
     }
