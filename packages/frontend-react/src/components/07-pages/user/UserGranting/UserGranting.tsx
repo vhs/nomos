93eb0eef08fdcb65fcd565d24be30b94b4a61d61
Ty Eggen <tycho.eggen@gmail.com>
2025-02-10 09:39:39 +0000

    feat(frontend): under construction banner on user granting page

--- packages/frontend-react/src/components/07-pages/user/UserGranting/UserGranting.tsx
+++ packages/frontend-react/src/components/07-pages/user/UserGranting/UserGranting.tsx
@@ -2,11 +2,12 @@ import type { FC } from 'react'
 
 import type { UserGrantingProps } from './UserGranting.types'
 
+import UnderConstructionBanner from '@/components/01-atoms/UnderConstructionBanner/UnderConstructionBanner'
 import BasePage from '@/components/05-materials/BasePage/BasePage'
 
 const UserGranting: FC<UserGrantingProps> = () => (
     <BasePage data-testid='UserGranting' title='Grant Privileges'>
-        Grant Privileges
+        <UnderConstructionBanner />
     </BasePage>
 )
 
