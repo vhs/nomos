8c4996d5e489adaaab60c1e3bf4c6caa9f42de3f
Ty Eggen <tycho.eggen@gmail.com>
2025-02-12 00:30:00 +0000

    style(frontend): move to more universal data-row css class for table handling

--- packages/frontend-react/src/components/01-atoms/TablePageRow/TablePageRow.tsx
+++ packages/frontend-react/src/components/01-atoms/TablePageRow/TablePageRow.tsx
@@ -3,7 +3,7 @@ import type { FC } from 'react'
 import type { TablePageRowProps } from './TablePageRow.types'
 
 const TablePageRow: FC<TablePageRowProps> = ({ children }) => (
-    <tr className='tablepage-row' data-testid='TablePageRow'>
+    <tr className='data-row' data-testid='TablePageRow'>
         {children}
     </tr>
 )
