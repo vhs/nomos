0c3ec9c2dfaa08dae7e321dd809d228e333f6625
Ty Eggen <tycho.eggen@gmail.com>
2025-02-12 00:19:02 +0000

    fix(frontend): AdminUsersEdit route component path

--- packages/frontend-react/src/routes/_admin/admin.users.$userId.tsx
+++ packages/frontend-react/src/routes/_admin/admin.users.$userId.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-import AdminUsersEdit from '@/components/07-pages/admin/AdminUsersEdit/AdminUsersEdit.lazy'
+import AdminUsersEdit from '@/components/07-pages/admin/AdminUsers/AdminUsersEdit/AdminUsersEdit.lazy'
 
 export const Route = createFileRoute('/_admin/admin/users/$userId')({
     component: AdminUsersEdit
