a369d740413ba93d4a2cd7cad9719e7cd9226fba
Ty Eggen <tycho.eggen@gmail.com>
2025-02-18 22:22:01 +0000

    feat(frontend): MappedArray utility type

--- packages/frontend-react/src/types/utils.ts
+++ packages/frontend-react/src/types/utils.ts
@@ -5,3 +5,7 @@ export type ArrayElement<T> = T extends Array<infer U> ? U : never
 export type SingleOrArray<T> = T | T[]
 
 export type CastReactElement<K extends keyof JSX.IntrinsicElements> = JSX.IntrinsicElements[K]
+
+export type MappedArray<T extends readonly string[]> = {
+    [K in keyof T]: T[K]
+}
