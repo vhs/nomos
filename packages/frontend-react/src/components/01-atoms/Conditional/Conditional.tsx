227148acf365fb8efcef104f627bb80a9d499ac3
Ty Eggen <tycho.eggen@gmail.com>
2025-02-24 08:33:29 +0000

    dev(frontend): Conditional promise error message suppression

--- packages/frontend-react/src/components/01-atoms/Conditional/Conditional.tsx
+++ packages/frontend-react/src/components/01-atoms/Conditional/Conditional.tsx
@@ -2,6 +2,7 @@ import type { FC } from 'react'
 
 import type { ConditionalProps } from './Conditional.types'
 
+// eslint-disable-next-line @typescript-eslint/promise-function-async
 const Conditional: FC<ConditionalProps> = ({ condition, fallback, children }) => {
     if (!condition) return fallback ?? null
 
