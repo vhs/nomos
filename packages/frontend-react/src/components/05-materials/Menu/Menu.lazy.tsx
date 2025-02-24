d88648846f6b36247d8ebb257e71327dcc79ca2a
Ty Eggen <tycho.eggen@gmail.com>
2025-02-24 08:35:19 +0000

    feat(frontend): improved Menu and MobileMenu handling

--- packages/frontend-react/src/components/05-materials/Menu/Menu.lazy.tsx
+++ packages/frontend-react/src/components/05-materials/Menu/Menu.lazy.tsx
@@ -1,12 +1,12 @@
 import { lazy, Suspense, type JSX } from 'react'
 
-import type { UserMenuProps } from './Menu.types'
+import type { MenuProps } from './Menu.types'
 
 import LoadingOverlay from '@/components/02-molecules/LoadingOverlay/LoadingOverlay'
 
 const LazyMenu = lazy(async () => await import('./Menu'))
 
-const Menu = (props: JSX.IntrinsicAttributes & UserMenuProps): JSX.Element => (
+const Menu = (props: JSX.IntrinsicAttributes & MenuProps): JSX.Element => (
     <Suspense fallback={<LoadingOverlay show={true} />}>
         <LazyMenu {...props} />
     </Suspense>
