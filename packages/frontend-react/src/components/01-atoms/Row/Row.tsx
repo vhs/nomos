6c1349f5ba7c8275f0c0cfd86d20adc603404aed
Ty Eggen <tycho.eggen@gmail.com>
2025-02-24 08:33:57 +0000

    feat(frontend): improved Row handling

--- packages/frontend-react/src/components/01-atoms/Row/Row.tsx
+++ packages/frontend-react/src/components/01-atoms/Row/Row.tsx
@@ -4,10 +4,18 @@ import { clsx } from 'clsx'
 
 import type { RowProps } from './Row.types'
 
-const Row: FC<RowProps> = ({ children, className, ...restProps }) => (
-    <div className={clsx(['row', className])} data-testid='Row' {...restProps}>
-        {children}
-    </div>
-)
+const Row: FC<RowProps> = ({ children, className, noWrap, ...restProps }) => {
+    noWrap ??= false
+
+    return (
+        <div
+            className={clsx(['row', className, noWrap ? 'flex-nowrap' : 'flex-wrap'])}
+            data-testid='Row'
+            {...restProps}
+        >
+            {children}
+        </div>
+    )
+}
 
 export default Row
