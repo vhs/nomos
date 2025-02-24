afc14440dbaec8311f40081d35020954656e5058
Ty Eggen <tycho.eggen@gmail.com>
2025-02-24 08:26:54 +0000

    feat(frontend): shared AuthenticatedLayout component for User/Admin layouts

---
+++ packages/frontend-react/src/components/06-layouts/AuthenticatedLayout/AuthenticatedLayout.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import AuthenticationProvider from '@/components/09-providers/AuthenticationProvider/AuthenticationProvider'
+
+import AuthenticatedLayout from './AuthenticatedLayout'
+
+type StoryType = StoryObj<typeof AuthenticatedLayout>
+
+const meta: Meta<typeof AuthenticatedLayout> = {
+    component: AuthenticatedLayout,
+    title: '99-Templates/default',
+    decorators: [
+        (Story) => (
+            <AuthenticationProvider>
+                <Story />
+            </AuthenticationProvider>
+        )
+    ]
+}
+
+export default meta
+
+export const Default: StoryType = {
+    args: {
+        children: 'AuthenticatedLayout'
+    }
+}
