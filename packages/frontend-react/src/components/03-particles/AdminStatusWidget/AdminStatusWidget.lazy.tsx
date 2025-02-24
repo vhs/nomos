65380a47ab48bf5914b30e2b463b399c3be02ae9
Ty Eggen <tycho.eggen@gmail.com>
2025-02-24 06:52:07 +0000

    feat(frontend): AdminStatusWidget (for AdminDashboard)

---
+++ packages/frontend-react/src/components/03-particles/AdminStatusWidget/AdminStatusWidget.lazy.tsx
@@ -0,0 +1,15 @@
+import { lazy, Suspense, type JSX } from 'react'
+
+import type { AdminStatusWidgetProps } from './AdminStatusWidget.types'
+
+import LoadingOverlay from '@/components/02-molecules/LoadingOverlay/LoadingOverlay'
+
+const LazyAdminStatusWidget = lazy(async () => await import('./AdminStatusWidget'))
+
+const AdminStatusWidget = (props: JSX.IntrinsicAttributes & AdminStatusWidgetProps): JSX.Element => (
+    <Suspense fallback={<LoadingOverlay show={true} />}>
+        <LazyAdminStatusWidget {...props} />
+    </Suspense>
+)
+
+export default AdminStatusWidget
