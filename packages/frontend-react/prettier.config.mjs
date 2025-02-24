9bd5a73f93ae39641edbe07802146da50b9131ce
Ty Eggen <tycho.eggen@gmail.com>
2025-02-24 08:31:30 +0000

    dev(frontend): improved tailwind handling

--- packages/frontend-react/prettier.config.mjs
+++ packages/frontend-react/prettier.config.mjs
@@ -2,4 +2,11 @@ import config from '../../prettier.config.mjs'
 
 config.printWidth = 120
 
+config.overrides.push({
+    files: ['*.css'],
+    options: {
+        parser: 'tailwind'
+    }
+})
+
 export default config
