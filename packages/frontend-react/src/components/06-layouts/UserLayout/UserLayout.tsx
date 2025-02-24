7340d569476f27958bda8bb235db5b0fc9093b99
Ty Eggen <tycho.eggen@gmail.com>
2025-02-24 08:26:54 +0000

    feat(frontend): shared AuthenticatedLayout component for User/Admin layouts

--- packages/frontend-react/src/components/06-layouts/UserLayout/UserLayout.tsx
+++ packages/frontend-react/src/components/06-layouts/UserLayout/UserLayout.tsx
@@ -1,32 +1,14 @@
 import type { FC } from 'react'
 
-import { Outlet } from '@tanstack/react-router'
-
 import type { UserLayoutProps } from './UserLayout.types'
 
-import Col from '@/components/01-atoms/Col/Col'
-import Container from '@/components/01-atoms/Container/Container'
-import Row from '@/components/01-atoms/Row/Row'
 import UserGuard from '@/components/02-molecules/UserGuard/UserGuard'
-import Menu from '@/components/05-materials/Menu/Menu'
-import MobileMenu from '@/components/05-materials/MobileMenu/MobileMenu'
+import AuthenticatedLayout from '@/components/06-layouts/AuthenticatedLayout/AuthenticatedLayout'
 
 import { UserMenuItems } from './UserLayout.ui'
 
 const UserLayout: FC<UserLayoutProps> = () => (
-    <UserGuard>
-        <Container fluid>
-            <Row className='main-layout-row'>
-                <Col className='basis-dynamic-menu'>
-                    <Menu className='nav' menuItems={UserMenuItems} />
-                </Col>
-                <Col className='basis-dynamic-content'>
-                    <Outlet />
-                </Col>
-            </Row>
-            <MobileMenu menuItems={UserMenuItems} />
-        </Container>
-    </UserGuard>
+    <AuthenticatedLayout guardComponent={UserGuard} menuItems={UserMenuItems} />
 )
 
 export default UserLayout
