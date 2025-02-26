663595955783eb4a026a865317faeed246fb96e4
Ty Eggen <tycho.eggen@gmail.com>
2025-02-26 18:12:43 +0000

    clean(frontend): retired StatusSelectorCard

--- packages/frontend-react/src/components/04-composites/StatusSelectorCard/StatusSelectorCard.utils.ts
+++
@@ -1,8 +0,0 @@
-import type { UserActiveStateCodes, UserActiveStateTitles } from '@/types/common'
-
-export const statuses: Record<UserActiveStateCodes, UserActiveStateTitles> = {
-    y: 'Active',
-    n: 'Inactive',
-    t: 'Pending',
-    b: 'Banned'
-}
