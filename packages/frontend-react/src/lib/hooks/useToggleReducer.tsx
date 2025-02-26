cc8c75c93beae935d3fb06a8a22287c79b764226
Ty Eggen <tycho.eggen@gmail.com>
2025-02-26 18:27:04 +0000

    refactor(frontend): optimized useToggleReducer hook

--- packages/frontend-react/src/lib/hooks/useToggleReducer.tsx
+++ packages/frontend-react/src/lib/hooks/useToggleReducer.tsx
@@ -1,23 +1,10 @@
-/* eslint-disable @typescript-eslint/no-loop-func */
-import { type ActionDispatch, type Dispatch, type SetStateAction, useCallback, useReducer, useState } from 'react'
-
-import { convertStringArrayToBooleanRecord } from '@/lib/utils'
+import { type ActionDispatch, useMemo, useReducer } from 'react'
 
 import type { BooleanRecord } from '@/types/common'
 
-export interface RefreshBooleanRecordAction {
-    action: 'refresh'
-    value: BooleanRecord
-}
-
-export interface ReplaceAllBooleanRecordAction {
-    action: 'replace'
-    value: string[]
-}
-
 export interface ResetBooleanRecordAction {
     action: 'reset'
-    value: never
+    value?: never
 }
 
 export interface SetAllBooleanRecordAction {
@@ -27,12 +14,12 @@ export interface SetAllBooleanRecordAction {
 
 export interface SetBooleanRecordAction {
     action: 'set'
-    value?: string
+    value: string
 }
 
 export interface ToggleBooleanRecordAction {
     action: 'toggle'
-    value?: string
+    value: string
 }
 
 export interface UnsetAllBooleanRecordAction {
@@ -45,102 +32,137 @@ export interface UnsetBooleanRecordAction {
     value: string
 }
 
+export interface UpdateDefaultsBooleanRecordAction {
+    action: 'update-defaults'
+    value: BooleanRecord
+}
+
 export type BooleanRecordMutationArg =
-    | RefreshBooleanRecordAction
-    | ReplaceAllBooleanRecordAction
     | ResetBooleanRecordAction
     | SetAllBooleanRecordAction
     | SetBooleanRecordAction
     | ToggleBooleanRecordAction
     | UnsetAllBooleanRecordAction
     | UnsetBooleanRecordAction
+    | UpdateDefaultsBooleanRecordAction
 
-export type BooleanRecordReducerHook = [BooleanRecord, ActionDispatch<[BooleanRecordMutationArg]>]
+interface BooleanRecordStore {
+    state: BooleanRecord
+    isDirty: boolean
+    defaultValues: BooleanRecord
+}
 
-export const booleanStateRecordReducer = (
-    state: BooleanRecord,
+export interface BooleanRecordReducerHook {
+    state: BooleanRecord
+    dispatch: ActionDispatch<[BooleanRecordMutationArg]>
+    isDirty: boolean
+}
+
+export const booleanStateRecordStoreReducer = (
+    store: BooleanRecordStore,
     ...mutations: BooleanRecordMutationArg[]
-): BooleanRecord => {
-    let updateState = structuredClone(state)
+): BooleanRecordStore => {
+    const updateStore = structuredClone(store)
 
     for (const mutation of mutations) {
         const { action, value } = mutation
 
         switch (action) {
-            case 'refresh':
-                updateState = value
+            case 'reset': {
+                updateStore.state = structuredClone(store.defaultValues)
+
+                updateStore.isDirty = false
+
                 break
-            case 'replace':
-                updateState = convertStringArrayToBooleanRecord(value)
-                break
-            case 'set-all':
-                Object.keys(updateState).forEach((e) => {
-                    updateState[e] = true
+            }
+            case 'set-all': {
+                let dirties = 0
+
+                Object.keys(updateStore.state).forEach((e) => {
+                    updateStore.state = { ...updateStore.state, [e]: true }
+
+                    if (updateStore.state[e] !== updateStore.defaultValues[e]) dirties++
                 })
+
+                updateStore.isDirty = dirties > 0
+
                 break
-            case 'set':
+            }
+            case 'set': {
                 if (typeof value !== 'string') throw new Error('Invalid toggle value')
-                updateState[value] = true
+
+                updateStore.state = { ...updateStore.state, [value]: true }
+
+                updateStore.isDirty = updateStore.state[value] !== updateStore.defaultValues[value]
+
                 break
-            case 'toggle':
+            }
+            case 'toggle': {
                 if (typeof value !== 'string') throw new Error('Invalid toggle value')
-                updateState[value] = !updateState[value]
+
+                updateStore.state = { ...updateStore.state, [value]: !updateStore.state[value] }
+
+                updateStore.isDirty = updateStore.state[value] !== updateStore.defaultValues[value]
+
                 break
-            case 'unset-all':
-                Object.keys(updateState).forEach((e) => {
-                    updateState[e] = false
+            }
+            case 'unset-all': {
+                let dirties = 0
+
+                Object.keys(updateStore).forEach((e) => {
+                    updateStore.state = { ...updateStore.state, [e]: false }
+
+                    if (updateStore.state[e] !== updateStore.defaultValues[e]) dirties++
                 })
+
+                updateStore.isDirty = dirties > 0
+
                 break
-            case 'unset':
+            }
+            case 'unset': {
                 if (typeof value !== 'string') throw new Error('Invalid toggle value')
-                updateState[value] = false
+
+                updateStore.state = { ...updateStore.state, [value]: false }
+
+                updateStore.isDirty = updateStore.state[value] !== updateStore.defaultValues[value]
+
                 break
+            }
+            case 'update-defaults': {
+                updateStore.defaultValues = value
+
+                break
+            }
             default:
-                throw new Error('Unknown action')
+                // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                throw new Error(`Unknown action: ${action}`)
         }
     }
 
-    return updateState
+    return updateStore
 }
 
-const generateInitialState = (defaultBooleanRecord?: BooleanRecord): BooleanRecord => defaultBooleanRecord ?? {}
+const generateInitialStore = (defaultBooleanRecord?: BooleanRecord): BooleanRecordStore => ({
+    state: defaultBooleanRecord ?? {},
+    isDirty: false,
+    defaultValues: defaultBooleanRecord ?? {}
+})
 
-const useToggleReducer = (
-    defaultBooleanRecord?: BooleanRecord
-): [
-    BooleanRecord,
-    ActionDispatch<[BooleanRecordMutationArg]>,
-    boolean,
-    Dispatch<SetStateAction<BooleanRecord | undefined>>
-] => {
-    const [isDirty, setDirty] = useState(false)
-    const [defaultState, updateDefaults] = useState<BooleanRecord | undefined>(defaultBooleanRecord)
-
-    const [booleanRecord, dispatchBooleanRecord] = useReducer<BooleanRecord, [BooleanRecordMutationArg]>(
-        booleanStateRecordReducer,
+const useToggleReducer = (defaultBooleanRecord?: BooleanRecord): BooleanRecordReducerHook => {
+    const [booleanRecordStore, dispatchBooleanRecord] = useReducer<BooleanRecordStore, [BooleanRecordMutationArg]>(
+        booleanStateRecordStoreReducer,
         defaultBooleanRecord,
         // @ts-expect-error unexpected third arg - typedef sucks
-        generateInitialState
+        generateInitialStore
     )
 
-    const dispatchBooleanRecordWrapper = useCallback(
-        (m: BooleanRecordMutationArg) => {
-            if (m.action === 'reset' && defaultState != null) {
-                m = {
-                    action: 'refresh',
-                    value: defaultState
-                }
-                setDirty(false)
-            } else {
-                setDirty(true)
-            }
+    const reducerObj = useMemo(() => {
+        const { state, isDirty } = booleanRecordStore
 
-            dispatchBooleanRecord(m)
-        },
-        [defaultState]
-    )
+        return { state, dispatch: dispatchBooleanRecord, isDirty }
+    }, [booleanRecordStore])
 
-    return [booleanRecord, dispatchBooleanRecordWrapper, isDirty, updateDefaults]
+    return reducerObj
 }
 
 export default useToggleReducer
