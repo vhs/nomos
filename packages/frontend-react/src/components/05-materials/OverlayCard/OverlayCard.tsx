d8939dfb220b7b4f88c6483c7c7b80c97d5521b1
Ty Eggen <tycho.eggen@gmail.com>
2025-02-26 23:22:48 +0000

    feat(frontend): optimize Overlay/OverlayCard

--- packages/frontend-react/src/components/05-materials/OverlayCard/OverlayCard.tsx
+++ packages/frontend-react/src/components/05-materials/OverlayCard/OverlayCard.tsx
@@ -1,6 +1,6 @@
 import type { FC } from 'react'
 
-import { redirect, useRouter } from '@tanstack/react-router'
+import { redirect, useLocation, useRouter } from '@tanstack/react-router'
 import { clsx } from 'clsx'
 
 import type { OverlayCardProps } from './OverlayCard.types'
@@ -10,37 +10,53 @@ import Conditional from '@/components/01-atoms/Conditional/Conditional'
 import Overlay from '@/components/01-atoms/Overlay/Overlay'
 import Card from '@/components/04-composites/Card/Card'
 
-const OverlayCard: FC<OverlayCardProps> = ({ show, title, children, className, actions, closeLabel, onClose }) => {
-    closeLabel ??= 'Cancel'
+const OverlayCard: FC<OverlayCardProps> = ({
+    show,
+    title,
+    children,
+    className,
+    actions,
+    closeLabel,
+    closeVariant,
+    onClose,
+    redirectTo
+}) => {
+    closeLabel ??= actions != null ? 'Cancel' : 'Close'
+    closeVariant ??= actions != null ? 'warning' : 'primary'
     show ??= true
 
     const router = useRouter()
 
+    const pathname = useLocation({
+        select: (location) => location.pathname
+    })
+
     const closeHandler = (): void => {
-        if (onClose == null || onClose?.())
-            router.history.length > 0 ? router.history.back() : redirect({ to: '/admin' })
+        if (onClose == null || onClose?.()) {
+            if (redirectTo != null) redirect({ to: redirectTo })
+            else if (router.history.length > 0) router.history.back()
+            else redirect({ to: pathname.split('/').slice(0, 2).join('/') })
+        }
     }
 
     return (
         <div data-testid='OverlayCard'>
             <Conditional condition={show}>
                 <Overlay handler={closeHandler}>
-                    <div className={clsx([className, 'm-auto'])}>
-                        <Card>
-                            <Card.Header>
-                                <h1>{title}</h1>
-                            </Card.Header>
+                    <Card className={clsx([className])}>
+                        <Card.Header>
+                            <h1>{title}</h1>
+                        </Card.Header>
 
-                            <Card.Body className='max-h-[60vh] overflow-y-scroll'>{children}</Card.Body>
+                        <Card.Body className='max-h-[60vh] overflow-y-scroll'>{children}</Card.Body>
 
-                            <Card.Footer>
-                                {actions}
-                                <Button variant='warning' onClick={closeHandler}>
-                                    {closeLabel}
-                                </Button>
-                            </Card.Footer>
-                        </Card>
-                    </div>
+                        <Card.Footer>
+                            {actions}
+                            <Button variant={closeVariant} onClick={closeHandler}>
+                                {closeLabel}
+                            </Button>
+                        </Card.Footer>
+                    </Card>
                 </Overlay>
             </Conditional>
         </div>
