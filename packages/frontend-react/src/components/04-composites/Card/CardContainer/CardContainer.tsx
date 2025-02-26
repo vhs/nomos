e5839ac7e439d446387a039f6566e462907cd968
Ty Eggen <tycho.eggen@gmail.com>
2025-02-26 19:48:42 +0000

    feat(frontend): CardContainer error state support

--- packages/frontend-react/src/components/04-composites/Card/CardContainer/CardContainer.tsx
+++ packages/frontend-react/src/components/04-composites/Card/CardContainer/CardContainer.tsx
@@ -4,10 +4,18 @@ import clsx from 'clsx'
 
 import type { CardContainerProps } from './CardContainer.types'
 
-const CardContainer: FC<CardContainerProps> = ({ children, className, ...restProps }) => (
-    <div className={clsx([className, 'card'])} data-testid='CardContainer' {...restProps}>
-        {children}
-    </div>
-)
+const CardContainer: FC<CardContainerProps> = ({ children, className, error, ...restProps }) => {
+    error ??= false
+
+    return (
+        <div
+            className={clsx([className, 'card', error ? 'shadow-form-error' : null])}
+            data-testid='CardContainer'
+            {...restProps}
+        >
+            {children}
+        </div>
+    )
+}
 
 export default CardContainer
