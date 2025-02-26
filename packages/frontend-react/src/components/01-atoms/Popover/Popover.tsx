4955f710daa97b6ab0d70d4259a311acb1cef0b3
Ty Eggen <tycho.eggen@gmail.com>
2025-02-26 18:05:24 +0000

    feat(frontend): improved Popover styling

--- packages/frontend-react/src/components/01-atoms/Popover/Popover.tsx
+++ packages/frontend-react/src/components/01-atoms/Popover/Popover.tsx
@@ -5,10 +5,10 @@ import { clsx } from 'clsx'
 import type { PopoverProps } from './Popover.types'
 
 const Popover: FC<PopoverProps> = ({ className, content, popover }) => (
-    <div className={clsx(['popover', className])} data-testid='Popover'>
-        <div className={clsx(['peer w-full overflow-hidden text-ellipsis'])}>{content}</div>
+    <div className={clsx([className, 'popover inline'])} data-testid='Popover'>
+        <div className={clsx(['popover-content peer'])}>{content}</div>
         <div className='absolute z-above-and-beyond hidden peer-hover:block'>
-            <div className='relative bottom-12 rounded-md border border-black/50 bg-white p-1'>{popover}</div>
+            <div className='relative bottom-12 rounded-md border border-black/50 bg-amber-50 p-1'>{popover}</div>
         </div>
     </div>
 )
