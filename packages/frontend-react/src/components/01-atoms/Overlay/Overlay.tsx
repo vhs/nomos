7089f477944772b50704bcb3a9de04db34094183
Ty Eggen <tycho.eggen@gmail.com>
2025-02-26 23:22:48 +0000

    feat(frontend): optimize Overlay/OverlayCard

--- packages/frontend-react/src/components/01-atoms/Overlay/Overlay.tsx
+++ packages/frontend-react/src/components/01-atoms/Overlay/Overlay.tsx
@@ -2,7 +2,7 @@ import { useCallback, useEffect, type FC } from 'react'
 
 import type { OverlayProps } from './Overlay.types'
 
-import { useOutsideClick } from '@/lib/hooks/useClickOutside'
+import useOutsideClick from '@/lib/hooks/useClickOutside'
 
 const Overlay: FC<OverlayProps> = ({ children, handler }) => {
     const clickHandler = useCallback(() => {
@@ -20,11 +20,9 @@ const Overlay: FC<OverlayProps> = ({ children, handler }) => {
     }, [])
 
     return (
-        <div className='modal' data-testid='Overlay'>
-            <div className='modal-container'>
-                <div className='modal-content' ref={ref}>
-                    {children}
-                </div>
+        <div className='fixed bottom-0 left-0 right-0 top-0 z-above-and-beyond bg-black/10' data-testid='Overlay'>
+            <div className='m-auto mb-auto mt-[10vh] max-h-[80dvh] w-full md:w-[800px]' ref={ref}>
+                {children}
             </div>
         </div>
     )
