7f3035ca58065882ab1fd4f4425c265067f661d9
Ty Eggen <tycho.eggen@gmail.com>
2025-02-26 20:22:34 +0000

    fix(frontend): missing attributes in tests and stories

--- packages/frontend-react/src/components/03-particles/AdminStatusWidget/AdminStatusWidget.test.tsx
+++ packages/frontend-react/src/components/03-particles/AdminStatusWidget/AdminStatusWidget.test.tsx
@@ -5,6 +5,6 @@ import AdminStatusWidget from './AdminStatusWidget'
 it('It should mount', () => {
     const container = document.createElement('div')
     const root = createRoot(container)
-    root.render(<AdminStatusWidget />)
+    root.render(<AdminStatusWidget icon={'0'} count={0} description={'test'} details={'test'} />)
     root.unmount()
 })
--- packages/frontend-react/src/components/04-composites/SelectorCard/SelectorCard.test.tsx
+++ packages/frontend-react/src/components/04-composites/SelectorCard/SelectorCard.test.tsx
@@ -5,6 +5,6 @@ import SelectorCard from './SelectorCard'
 it('It should mount', () => {
     const container = document.createElement('div')
     const root = createRoot(container)
-    root.render(<SelectorCard />)
+    root.render(<SelectorCard id={'test'} mode={'radio'} options={{ test1: 'test1', test2: 'test2' }} title={'test'} />)
     root.unmount()
 })
