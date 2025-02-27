a91112aedc58544dc8eccaaf4ea8e0cbbf888349
Ty Eggen <tycho.eggen@gmail.com>
2025-02-27 17:55:51 +0000

    dev(frontend): removed redundant tailwind definition from frontend prettier config

--- packages/frontend-react/prettier.config.mjs
+++ packages/frontend-react/prettier.config.mjs
@@ -2,11 +2,4 @@ import config from '../../prettier.config.mjs'
 
 config.printWidth = 120
 
-config.overrides.push({
-    files: ['*.css'],
-    options: {
-        parser: 'tailwind'
-    }
-})
-
 export default config
