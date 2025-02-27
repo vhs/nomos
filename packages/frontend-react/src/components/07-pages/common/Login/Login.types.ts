7d5059f4bd64a60e356e6658bf572963923f000f
Ty Eggen <tycho.eggen@gmail.com>
2025-02-27 17:54:21 +0000

    feat(frontend): migrated the login page to react-hook-form

--- packages/frontend-react/src/components/07-pages/common/Login/Login.types.ts
+++ packages/frontend-react/src/components/07-pages/common/Login/Login.types.ts
@@ -1,5 +1,10 @@
 import type { ReactNode } from 'react'
 
+import type { LoginSchema } from './Login.schema'
+import type { z } from 'zod'
+
 export interface LoginProps {
     children?: ReactNode
 }
+
+export type LoginForm = z.infer<typeof LoginSchema>
