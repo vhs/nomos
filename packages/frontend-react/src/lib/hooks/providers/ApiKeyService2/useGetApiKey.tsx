bfb23b033f5926d4e891aec1daaebf2ef4b7e05b
Ty Eggen <tycho.eggen@gmail.com>
2025-02-28 23:35:01 +0000

    feat(frontend): useGetApiKey hook

--- packages/frontend-react/src/lib/hooks/providers/ApiKeyService2/useGetApiKey.tsx
+++ packages/frontend-react/src/lib/hooks/providers/ApiKeyService2/useGetApiKey.tsx
@@ -1,4 +1,8 @@
-/* eslint-disable */
-const useGetApiKey = () => {}
+import useSWR, { type SWRResponse } from 'swr'
+
+import type { Key } from '@/types/records'
+
+const useGetApiKey = (keyId?: number): SWRResponse<Key> =>
+    useSWR<Key>(keyId != null ? `/services/v2/ApiKeyService2.svc/GetApiKey?keyid=${keyId}` : null)
 
 export default useGetApiKey
