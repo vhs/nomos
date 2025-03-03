70e054aa453ee50ec5ab8bf535e50e875c5692f7
Ty Eggen <tycho.eggen@gmail.com>
2025-03-03 01:57:56 +0000

    fix(frontend): ApiKeysPageContainer test

--- packages/frontend-react/src/components/05-materials/ApiKeysPage/ApiKeysPageContainer/ApiKeysPageContainer.test.tsx
+++ packages/frontend-react/src/components/05-materials/ApiKeysPage/ApiKeysPageContainer/ApiKeysPageContainer.test.tsx
@@ -5,6 +5,20 @@ import ApiKeysPageContainer from './ApiKeysPageContainer'
 it('It should mount', () => {
     const container = document.createElement('div')
     const root = createRoot(container)
-    root.render(<ApiKeysPageContainer />)
+    root.render(
+        <ApiKeysPageContainer
+            basePath={'/apikeys'}
+            contextValue={{
+                activeView: 'list',
+                availableKeys: [],
+                availablePrivileges: [],
+                basePath: '/apikeys',
+                scope: 'user'
+            }}
+            createModal={false}
+            editModal={false}
+            activeView={'list'}
+        />
+    )
     root.unmount()
 })
