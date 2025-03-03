1ee64053d8eaf2aa1bd0b28041fefca5a40af95e
Ty Eggen <tycho.eggen@gmail.com>
2025-03-03 01:23:40 +0000

    clean(frontend): retired PathTabs component

--- packages/frontend-react/src/components/04-composites/PathTabs/PathTab/PathTab.types.ts
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from 'react'
-
-import type { CastReactElement } from '@/types/utils'
-
-export interface PathTabProps extends CastReactElement<'div'> {
-    children?: ReactNode
-    path: string
-    title: string
-}
