596c62a68d9e5035fce029c369aaa0b81fd77cb1
Ty Eggen <tycho.eggen@gmail.com>
2025-03-11 00:39:50 +0000

    feat(frontend): useGetGrantUserPrivileges hook

--- packages/frontend-react/src/lib/hooks/providers/UserService2/useGetGrantUserPrivileges.tsx
+++ packages/frontend-react/src/lib/hooks/providers/UserService2/useGetGrantUserPrivileges.tsx
@@ -1,4 +1,28 @@
-/* eslint-disable */
-const useGetGrantUserPrivileges = () => {}
+import useSWR, { type SWRResponse } from 'swr'
+
+import { HTTPException } from '@/lib/exceptions/HTTPException'
+import { isStringStringRecord } from '@/lib/guards/common'
+import UserService2 from '@/lib/providers/UserService2'
+
+import type { StringStringRecord } from '@/types/validators/common'
+
+const useGetGrantUserPrivileges = (userId: number | null | undefined): SWRResponse<StringStringRecord> => {
+    return useSWR<Record<string, string>>(
+        userId != null ? '/services/v2/UserService2.svc/GetGrantUserPrivileges' : null,
+        async (_url: string): Promise<Record<string, string>> => {
+            if (userId == null) throw new Error('Invalid userId')
+
+            const result = await UserService2.getInstance().GetGrantUserPrivileges(userId)
+
+            if (!isStringStringRecord(result)) {
+                const error = new HTTPException('An error occurred while fetching the data.')
+                error.info = result
+                throw error
+            }
+
+            return result
+        }
+    )
+}
 
 export default useGetGrantUserPrivileges
