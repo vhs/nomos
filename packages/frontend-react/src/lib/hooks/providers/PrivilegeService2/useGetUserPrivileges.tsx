a0f7ad9ef3fc295e3545edadce764bab1d25f0c7
Ty Eggen <tycho.eggen@gmail.com>
2025-03-11 00:40:55 +0000

    feat(frontend): useGetUserPrivileges hook

--- packages/frontend-react/src/lib/hooks/providers/PrivilegeService2/useGetUserPrivileges.tsx
+++ packages/frontend-react/src/lib/hooks/providers/PrivilegeService2/useGetUserPrivileges.tsx
@@ -1,4 +1,30 @@
-/* eslint-disable */
-const useGetUserPrivileges = () => {}
+import { useMemo } from 'react'
+
+import useSWR, { type SWRResponse } from 'swr'
+
+import { HTTPException } from '@/lib/exceptions/HTTPException'
+import { isPrivileges } from '@/lib/guards/records'
+import PrivilegeService2 from '@/lib/providers/PrivilegeService2'
+
+import type { Privileges } from '@/types/validators/records'
+
+const useGetUserPrivileges = (userId?: number): SWRResponse<Privileges> => {
+    const getUserPrivilegesUrl = useMemo(
+        () => (userId != null ? `/services/v2/PrivilegeService2.svc/GetUserPrivileges?userid=${userId}` : null),
+        [userId]
+    )
+
+    return useSWR<Privileges>(getUserPrivilegesUrl, async (_url: string): Promise<Privileges> => {
+        const result = await PrivilegeService2.getInstance().GetUserPrivileges(Number(userId))
+
+        if (!isPrivileges(result)) {
+            const error = new HTTPException('An error occurred while fetching the data.')
+            error.info = result
+            throw error
+        }
+
+        return result
+    })
+}
 
 export default useGetUserPrivileges
