a2d70e28f8ae4fcee9e5257e65350e675d2539d5
Ty Eggen <tycho.eggen@gmail.com>
2025-03-11 00:39:31 +0000

    feat(frontend): useGetAllPrivileges hook

--- packages/frontend-react/src/lib/hooks/providers/PrivilegeService2/useGetAllPrivileges.tsx
+++ packages/frontend-react/src/lib/hooks/providers/PrivilegeService2/useGetAllPrivileges.tsx
@@ -1,15 +1,24 @@
 import useSWR, { type SWRResponse } from 'swr'
 
+import { HTTPException } from '@/lib/exceptions/HTTPException'
+import { isPrivileges } from '@/lib/guards/records'
 import PrivilegeService2 from '@/lib/providers/PrivilegeService2'
 
-import type { NOMOSSWRResponse } from '@/types/api'
 import type { Privileges } from '@/types/validators/records'
 
-const useGetAllPrivileges = (): SWRResponse<NOMOSSWRResponse<Privileges>> => {
-    return useSWR<NOMOSSWRResponse<Privileges>>(
+const useGetAllPrivileges = (): SWRResponse<Privileges> => {
+    return useSWR<Privileges>(
         '/services/v2/PrivilegeService2.svc/GetAllPrivileges',
-        async (_url: string): Promise<NOMOSSWRResponse<Privileges>> => {
-            return await PrivilegeService2.getInstance().GetAllPrivileges()
+        async (_url: string): Promise<Privileges> => {
+            const result = await PrivilegeService2.getInstance().GetAllPrivileges()
+
+            if (!isPrivileges(result)) {
+                const error = new HTTPException('An error occurred while fetching the data.')
+                error.info = result
+                throw error
+            }
+
+            return result
         }
     )
 }
