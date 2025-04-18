d07897e428323313a4b647c1dc96b3aa0ff7e343
Ty Eggen <tycho.eggen@gmail.com>
2025-04-18 10:42:00 -0700

    dev(backend): enabled no_unused_imports php-cs-fixer setting

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -71,7 +71,7 @@ return (new PhpCsFixer\Config())
         // 'no_trailing_comma_in_list_call' => true,
         // 'no_trailing_comma_in_singleline_array' => true,
         // 'no_unneeded_control_parentheses' => true,
-        // 'no_unused_imports' => true,
+        'no_unused_imports' => true,
         'no_whitespace_before_comma_in_array' => true,
         'no_whitespace_in_blank_line' => true,
         // 'normalize_index_brace' => true,
