79f107738e279e48ad215b19440546785bb3a940
Ty Eggen <tycho.eggen@gmail.com>
2025-04-20 14:35:06 -0700

    dev(frontend): improved vite config host overrides

--- packages/frontend-react/src/vite-env.d.ts
+++ packages/frontend-react/src/vite-env.d.ts
@@ -1,2 +1,14 @@
-/* eslint-disable @typescript-eslint/triple-slash-reference */
+/* eslint-disable @typescript-eslint/naming-convention */
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly NOMOS_FRONTEND_HOST?: string
+    readonly NOMOS_FRONTEND_CA_FILE?: string
+    readonly NOMOS_FRONTEND_CRT_FILE?: string
+    readonly NOMOS_FRONTEND_KEY_FILE?: string
+    readonly NOMOS_FRONTEND_BACKEND_URL?: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
