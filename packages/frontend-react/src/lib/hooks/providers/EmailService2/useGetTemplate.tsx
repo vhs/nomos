8861182321762eaac6bfe6720aa242ce9b01089d
Ty Eggen <tycho.eggen@gmail.com>
2025-04-23 09:29:50 -0700

    feat(frontend): initial useGetTemplate implementation

--- packages/frontend-react/src/lib/hooks/providers/EmailService2/useGetTemplate.tsx
+++ packages/frontend-react/src/lib/hooks/providers/EmailService2/useGetTemplate.tsx
@@ -1,4 +1,11 @@
-/* eslint-disable */
-const useGetTemplate = () => {}
+import useSWR, { type SWRResponse } from 'swr'
+
+import type { EmailTemplate } from '@/types/validators/records'
+
+const useGetTemplate = (templateId?: number): SWRResponse<EmailTemplate> => {
+    return useSWR<EmailTemplate>(
+        templateId != null ? `/services/v2/EmailService2.svc/GetTemplate?id=${templateId}` : null
+    )
+}
 
 export default useGetTemplate
