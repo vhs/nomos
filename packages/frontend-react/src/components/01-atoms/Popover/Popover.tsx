6cb60ba8ca4c96095856ecb77548d7d377e30374
Ty Eggen <tycho.eggen@gmail.com>
2025-04-23 09:17:54 -0700

    feat(frontend): make Popover content wrap

--- packages/frontend-react/src/components/01-atoms/Popover/Popover.tsx
+++ packages/frontend-react/src/components/01-atoms/Popover/Popover.tsx
@@ -8,7 +8,9 @@ const Popover: FC<PopoverProps> = ({ className, content, popover }) => (
     <div className={clsx([className, 'popover inline'])} data-testid='Popover'>
         <div className={clsx(['popover-content peer'])}>{content}</div>
         <div className='absolute z-above-and-beyond hidden peer-hover:block'>
-            <div className='relative bottom-12 rounded-md border border-black/50 bg-amber-50 p-1'>{popover}</div>
+            <div className='relative bottom-12 text-wrap rounded-md border border-black/50 bg-amber-50 p-1'>
+                {popover}
+            </div>
         </div>
     </div>
 )
