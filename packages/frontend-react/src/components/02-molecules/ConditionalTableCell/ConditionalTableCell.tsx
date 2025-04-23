7f53e5e30c1e43dd8a81bbce008fdc7eb5d3ce63
Ty Eggen <tycho.eggen@gmail.com>
2025-04-23 09:18:56 -0700

    feat(frontend): automatically make ConditionalTableCell content Popover based on string length

--- packages/frontend-react/src/components/02-molecules/ConditionalTableCell/ConditionalTableCell.tsx
+++ packages/frontend-react/src/components/02-molecules/ConditionalTableCell/ConditionalTableCell.tsx
@@ -5,10 +5,17 @@ import { clsx } from 'clsx'
 import type { ConditionalTableCellProps } from './ConditionalTableCell.types'
 
 import Conditional from '@/components/01-atoms/Conditional/Conditional'
+import Popover from '@/components/01-atoms/Popover/Popover'
 
 const ConditionalTableCell: FC<ConditionalTableCellProps> = ({ className, condition, children }) => (
     <Conditional condition={condition}>
-        <td className={clsx([className, 'px-1 text-center'])}>{children}</td>
+        <td className={clsx([className, 'px-1 text-center'])}>
+            {(typeof children === 'string' && children.length < 16) || typeof children !== 'string' ? (
+                children
+            ) : (
+                <Popover content={children} popover={children} />
+            )}
+        </td>
     </Conditional>
 )
 
