f4533c31b85102eba3ba2eda96ca528d5c0414b5
Ty Eggen <tycho.eggen@gmail.com>
2025-04-23 09:21:12 -0700

    feat(frontend): refactor table row/cell width scaling and allow cell width overrides by cols prop

--- packages/frontend-react/src/components/01-atoms/TablePageRow/TablePageRow.tsx
+++ packages/frontend-react/src/components/01-atoms/TablePageRow/TablePageRow.tsx
@@ -1,11 +1,21 @@
 import type { FC } from 'react'
 
+import { clsx } from 'clsx'
+
 import type { TablePageRowProps } from './TablePageRow.types'
 
-const TablePageRow: FC<TablePageRowProps> = ({ children }) => (
-    <tr className='data-row' data-testid='TablePageRow'>
-        {children}
-    </tr>
-)
+import { TablePageRowFieldsClasses } from './TablePageRow.utils'
+
+const TablePageRow: FC<TablePageRowProps> = ({ fields, children }) => {
+    fields ??= 0
+
+    const cssClass = TablePageRowFieldsClasses[fields] ?? 'data-fields-default'
+
+    return (
+        <tr className={clsx(['data-row', cssClass])} data-testid='TablePageRow'>
+            {children}
+        </tr>
+    )
+}
 
 export default TablePageRow
---
+++ packages/frontend-react/src/components/01-atoms/TablePageRow/TablePageRow.utils.ts
@@ -0,0 +1,15 @@
+export const TablePageRowFieldsClasses: Record<number, string> = {
+    0: 'data-fields-default',
+    1: 'data-fields-1',
+    2: 'data-fields-2',
+    3: 'data-fields-3',
+    4: 'data-fields-4',
+    5: 'data-fields-5',
+    6: 'data-fields-6',
+    7: 'data-fields-7',
+    8: 'data-fields-8',
+    9: 'data-fields-9',
+    10: 'data-fields-10',
+    11: 'data-fields-11',
+    12: 'data-fields-12'
+}
