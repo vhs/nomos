2b8b264059203bd39557f5628eedb17fd9af69eb
Ty Eggen <tycho.eggen@gmail.com>
2025-04-24 15:43:31 -0700

    chore(frontend): TODO comment for Popover component

--- packages/frontend-react/src/components/01-atoms/Popover/Popover.tsx
+++ packages/frontend-react/src/components/01-atoms/Popover/Popover.tsx
@@ -4,10 +4,12 @@ import { clsx } from 'clsx'
 
 import type { PopoverProps } from './Popover.types'
 
+// TODO make popover a clickable toggle to persist popover for easy interaction
+
 const Popover: FC<PopoverProps> = ({ className, content, popover }) => (
     <div className={clsx([className, 'popover inline'])} data-testid='Popover'>
         <div className={clsx(['popover-content peer'])}>{content}</div>
-        <div className='absolute z-above-and-beyond hidden peer-hover:block'>
+        <div className='absolute z-above-and-beyond hidden -translate-x-[50%] peer-hover:block'>
             <div className='relative bottom-12 text-wrap rounded-md border border-black/50 bg-amber-50 p-1'>
                 {popover}
             </div>
