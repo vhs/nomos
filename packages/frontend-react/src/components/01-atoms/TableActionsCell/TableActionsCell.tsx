f2a65980e41980d65161a7d0048fb8e9b2d4905d
Ty Eggen <tycho.eggen@gmail.com>
2025-04-24 15:44:25 -0700

    feat(frontend): TableActionCell className override support

--- packages/frontend-react/src/components/01-atoms/TableActionsCell/TableActionsCell.tsx
+++ packages/frontend-react/src/components/01-atoms/TableActionsCell/TableActionsCell.tsx
@@ -2,8 +2,8 @@ import type { FC } from 'react'
 
 import type { TableActionsCellProps } from './TableActionsCell.types'
 
-const TableActionsCell: FC<TableActionsCellProps> = ({ children }) => (
-    <td>
+const TableActionsCell: FC<TableActionsCellProps> = ({ className, children }) => (
+    <td className={className}>
         <div className='grid grid-flow-col justify-evenly'>{children}</div>
     </td>
 )
