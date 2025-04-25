edfb6d6af0d11c57d55eb724039159b8af0ce9c4
Ty Eggen <tycho.eggen@gmail.com>
2025-04-25 16:42:43 -0700

    feat(frontend): implemented useGetPRivilege

--- packages/frontend-react/src/lib/hooks/providers/PrivilegeService2/useGetPrivilege.tsx
+++ packages/frontend-react/src/lib/hooks/providers/PrivilegeService2/useGetPrivilege.tsx
@@ -1,4 +1,11 @@
-/* eslint-disable */
-const useGetPrivilege = () => {}
+import useSWR, { type SWRResponse } from 'swr'
+
+import type { Privilege } from '@/types/validators/records'
+
+const useGetPrivilege = (privilegeId?: number): SWRResponse<Privilege> => {
+    return useSWR<Privilege>(
+        privilegeId != null ? `/services/v2/PrivilegeService2.svc/GetPrivilege?id=${privilegeId}` : null
+    )
+}
 
 export default useGetPrivilege
