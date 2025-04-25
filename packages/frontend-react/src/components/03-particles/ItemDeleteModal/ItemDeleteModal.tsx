b2f1c7bf6bfdec68d014d0301d1d28bfe27f9b86
Ty Eggen <tycho.eggen@gmail.com>
2025-04-25 10:23:56 -0700

    feat(frontend): ItemDeleteModal overrideable title

--- packages/frontend-react/src/components/03-particles/ItemDeleteModal/ItemDeleteModal.tsx
+++ packages/frontend-react/src/components/03-particles/ItemDeleteModal/ItemDeleteModal.tsx
@@ -6,11 +6,11 @@ import Button from '@/components/01-atoms/Button/Button'
 import Conditional from '@/components/01-atoms/Conditional/Conditional'
 import OverlayCard from '@/components/05-materials/OverlayCard/OverlayCard'
 
-const ItemDeleteModal: FC<ItemDeleteModalProps> = ({ show, actionHandler, closeHandler, children }) => (
+const ItemDeleteModal: FC<ItemDeleteModalProps> = ({ show, actionHandler, closeHandler, title, children }) => (
     <div data-testid='ItemDeleteModal'>
         <Conditional condition={show}>
             <OverlayCard
-                title='Confirm Delete'
+                title={title ?? 'Confirm Delete'}
                 actions={[
                     <Button key='Delete' variant='danger' onClick={actionHandler}>
                         Delete
