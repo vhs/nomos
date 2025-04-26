a58b97755936296df07699469aa8b526942601d1
Ty Eggen <tycho.eggen@gmail.com>
2025-04-25 17:25:22 -0700

    fix(frontend): ItemDeleteModal test props

--- packages/frontend-react/src/components/03-particles/ItemDeleteModal/ItemDeleteModal.test.tsx
+++ packages/frontend-react/src/components/03-particles/ItemDeleteModal/ItemDeleteModal.test.tsx
@@ -5,6 +5,18 @@ import ItemDeleteModal from './ItemDeleteModal'
 it('It should mount', () => {
     const container = document.createElement('div')
     const root = createRoot(container)
-    root.render(<ItemDeleteModal />)
+    root.render(
+        <ItemDeleteModal
+            show={false}
+            actionHandler={function (): void {
+                throw new Error('Function not implemented.')
+            }}
+            closeHandler={function (): void {
+                throw new Error('Function not implemented.')
+            }}
+        >
+            Would you like to delete this?
+        </ItemDeleteModal>
+    )
     root.unmount()
 })
