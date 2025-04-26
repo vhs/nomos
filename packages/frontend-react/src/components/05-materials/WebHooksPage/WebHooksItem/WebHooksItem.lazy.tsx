6ca60bad47dfffaec9e21aa48a3dc5970cbdf722
Ty Eggen <tycho.eggen@gmail.com>
2025-04-25 19:17:52 -0700

    refactor(frontend): renamed WebHooksComponent to WebHooksItem

---
+++ packages/frontend-react/src/components/05-materials/WebHooksPage/WebHooksItem/WebHooksItem.lazy.tsx
@@ -0,0 +1,15 @@
+import { lazy, Suspense, type JSX } from 'react'
+
+import type { WebHooksItemProps } from './WebHooksItem.types'
+
+import LoadingOverlay from '@/components/02-molecules/LoadingOverlay/LoadingOverlay'
+
+const LazyWebHooksItem = lazy(async () => await import('./WebHooksItem'))
+
+const WebHooksItem = (props: JSX.IntrinsicAttributes & WebHooksItemProps): JSX.Element => (
+    <Suspense fallback={<LoadingOverlay show={true} />}>
+        <LazyWebHooksItem {...props} />
+    </Suspense>
+)
+
+export default WebHooksItem
--- packages/frontend-react/src/components/05-materials/WebHooksPage/WebHooksItem/WebHooksItem.types.ts
+++ packages/frontend-react/src/components/05-materials/WebHooksPage/WebHooksItem/WebHooksItem.types.ts
@@ -1,4 +1,4 @@
 import type { TablePageItemComponent } from '@/types/ui'
 import type { WebHook } from '@/types/validators/records'
 
-export interface WebHooksComponentProps extends TablePageItemComponent<WebHook> {}
+export interface WebHooksItemProps extends TablePageItemComponent<WebHook> {}
