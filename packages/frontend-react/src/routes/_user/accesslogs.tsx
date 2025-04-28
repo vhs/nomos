71839b364a72c7cfa6fb9e957eec3e1d85d9f77c
Ty Eggen <tycho.eggen@gmail.com>
2025-04-27 23:02:55 -0700

    feat(frontend): change user access history url

--- packages/frontend-react/src/routes/_user/accesslogs.tsx
+++ packages/frontend-react/src/routes/_user/accesslogs.tsx
@@ -2,6 +2,6 @@ import { createFileRoute } from '@tanstack/react-router'
 
 import AccessHistory from '@/components/07-pages/user/AccessHistory/AccessHistory.lazy'
 
-export const Route = createFileRoute('/_user/user/accesslogs')({
-    component: AccessHistory
+export const Route = createFileRoute('/_user/accesslogs')({
+  component: AccessHistory,
 })
