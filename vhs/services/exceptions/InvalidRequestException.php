97f256752ed204743690d4ea7b5cd97bf7b5b35e
Ty Eggen <tycho.eggen@gmail.com>
2025-05-01 09:27:42 -0700

    feat(backend): improved InvalidRequestException handling with default Client_Error_Method_Not_Allowed code

--- vhs/services/exceptions/InvalidRequestException.php
+++ vhs/services/exceptions/InvalidRequestException.php
@@ -10,6 +10,24 @@
 namespace vhs\services\exceptions;
 
 use vhs\exceptions\HttpException;
+use vhs\web\enums\HttpStatusCodes;
 
 /** @typescript */
-class InvalidRequestException extends HttpException {}
+class InvalidRequestException extends HttpException {
+    /**
+     * Construct the exception. Note: The message is NOT binary safe.
+     *
+     * @link https://php.net/manual/en/exception.construct.php
+     *
+     * @param string                         $message  [optional] The Exception message to throw
+     * @param \vhs\web\enums\HttpStatusCodes $code     [optional] The Exception code
+     * @param \Throwable|null                $previous [optional] The previous throwable used for the exception chaining
+     */
+    public function __construct(
+        $message = 'Invalid Request Exception',
+        HttpStatusCodes $code = HttpStatusCodes::Client_Error_Method_Not_Allowed,
+        $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
+}
