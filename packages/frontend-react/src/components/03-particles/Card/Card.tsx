4dac3b8ed1a4baa57d104ce8184ee30b3c53d880
Cory Alder <cory@crowselectromusic.com>
2025-11-14 21:02:41 -0800

    Working frontend-react + frontend-web

--- packages/frontend-react/src/components/03-particles/Card/Card.tsx
+++ packages/frontend-react/src/components/03-particles/Card/Card.tsx
@@ -5,14 +5,15 @@ import Container from './CardContainer/CardContainer'
 import Footer from './CardFooter/CardFooter'
 import Header from './CardHeader/CardHeader'
 
-const Card = structuredClone(Container)
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-Card.prototype.Body = Body
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-Card.prototype.Footer = Footer
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-Card.prototype.Header = Header
+// @ts-expect-error Property 'Container' does not exist on type 'FC<ModalContainerProps>'.
+Container.Body = Body
+// @ts-expect-error Property 'Container' does not exist on type 'FC<ModalContainerProps>'.
+Container.Container = Container
+// @ts-expect-error Property 'Container' does not exist on type 'FC<ModalContainerProps>'.
+Container.Footer = Footer
+// @ts-expect-error Property 'Container' does not exist on type 'FC<ModalContainerProps>'.
+Container.Header = Header
 
-export default Card as CardComponent
+export default Container as CardComponent
 
 export { Body, Container, Footer, Header }
