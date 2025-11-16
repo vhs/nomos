e8d7bac948419901fd4fdda79746f24e83dd011d
Cory Alder <cory@crowselectromusic.com>
2025-11-15 20:41:11 -0800

    prevent loading spinner from hanging ui

--- packages/frontend-react/src/components/03-particles/LoadingOverlay/LoadingOverlay.tsx
+++ packages/frontend-react/src/components/03-particles/LoadingOverlay/LoadingOverlay.tsx
@@ -8,7 +8,7 @@ const LoadingOverlay: FC<LoadingOverlayProps> = ({ show }) => {
     show = show ?? true
 
     return show ? (
-        <div className='absolute bottom-0 left-0 right-0 top-0 z-above-and-beyond overflow-hidden bg-black/10'>
+        <div className='bottom-0 left-0 right-0 top-0 z-above-and-beyond bg-black/10'>
             <div className='relative inset-1/2 h-[100px] w-[100px] -translate-x-[50px] -translate-y-[50px] opacity-100'>
                 <Loading />
             </div>
