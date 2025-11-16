adaa80e465d2654236351dd03c769143b5d7a1c4
Cory Alder <cory@crowselectromusic.com>
2025-11-15 21:59:57 -0800

    remove dockerfile generator

--- docker/dct/stages/webhooker-base.ts
+++
@@ -1,9 +0,0 @@
-import { DockerConfigTool, IStage } from '@tyisi/docker-config-tool-js'
-
-export const createWebHookerBaseStage = (dct: DockerConfigTool, { baseStage }: Record<string, IStage>): IStage => {
-    const webHookerBaseStage = dct.withStage({ from: baseStage, as: `webhooker-base` })
-
-    webHookerBaseStage.withRun(`apk --no-cache add bash`)
-
-    return webHookerBaseStage
-}
--- tools/generate-dockerfile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$(realpath "$0")")/.." || exit 255
-
-pnpm dlx tsx@latest docker/dct/generate-dockerfile.ts > docker-compose/Dockerfile
